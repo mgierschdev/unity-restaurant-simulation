526c0fdeca8369f6c4878597cbcd160a07976344
Maximiliam <maximiliamgierschmann@gmail.com>
2022-06-28 17:33:50 -0400

    Adding body movement with keystrokes

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,8 +6,16 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D body;
 
+    // To get the reference from the object
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
 
+    // To track the key presses, left, right, the update is checked on every frame
+    private void Update()
+    {
+        body.velocity = new Vector2(Input.GetAxis("Horizontal"), body.velocity.y); // To assign how fast or player is moving and which direction (speed 2 directions)
 
-     
-
+    }
 }
