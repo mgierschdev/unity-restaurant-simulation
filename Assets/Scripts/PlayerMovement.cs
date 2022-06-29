3128997fe510f67b6e43d02c0bcf139ba223aae4
Max <62764972+pacmax2@users.noreply.github.com>
2022-06-29 09:48:44 -0400

    Removing unused packages and folders

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -18,15 +18,29 @@ public class PlayerMovement : MonoBehaviour
     // To track the key presses, left, right, the update is checked on every frame
     private void Update()
     {
+        float horizontalInput = Input.GetAxis("Horizontal");
         // To assign how fast or player is moving and which direction (speed 2 directions)
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y); 
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y); 
+
+
+        // Changes the character direction depending on the key press
+        if(horizontalInput < 0.01f)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if(horizontalInput > -0.01f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        }
 
         // To be able to jump
         if (Input.GetKey(KeyCode.Space))
         {
             body.velocity = new Vector2(body.velocity.x, speed);
-        }         
+        }
 
-        
+
+
     }
 }
