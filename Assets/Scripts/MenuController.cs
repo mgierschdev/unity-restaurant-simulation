1c3db5498f5c6d67d6e3332349cf0b550d8fd32a
Max <62764972+pacmax2@users.noreply.github.com>
2022-06-30 20:30:24 -0400

    Changing menu controller

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// This class is in charge of controlling the different UI controllers
+
 public class MenuController : MonoBehaviour
 {
+    GameObject UI;
+    GameObject mainMenu;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+      //  Debug.Log(this.gameObject.transform.position.z);
+
+
+        Debug.Log(this.gameObject);
+
     }
 
     // Update is called once per frame
