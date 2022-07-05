9d51019bedc91397470c1a5a9aba6693f97bf7ce
Max <62764972+pacmax2@users.noreply.github.com>
2022-07-05 15:50:00 -0400

    Renaming

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// This class in charge of loading the game and prefabs
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //GameObject player = new Pla
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
