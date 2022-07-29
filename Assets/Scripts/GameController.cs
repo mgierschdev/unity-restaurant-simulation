a517fcfd510ac95192df66d8b0c4587c8811b9ff
maximo30 <mgierschdev@gmail.com>
2022-07-29 13:56:49 -0400

    adding objects to the grid

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -16,9 +16,9 @@ public class GameController : MonoBehaviour
         GameObject gameGridObject = gameObject.transform.Find(Settings.PREFAB_GAME_GRID).gameObject;
         GameGridController gridController = gameGridObject.GetComponent<GameGridController>();
 
-        for(int i = 0; i < 2; i++){
+        for(int i = 0; i < 3; i++){
             // Adding NPC object
-            Vector3 initPos = new Vector3(22, 30, 1);
+            Vector3 initPos = new Vector3(22, 29, 1);
             GameObject npcObject = Instantiate(Resources.Load(Settings.PREFAB_NPC, typeof(GameObject)), gridController.GetCellPosition(initPos), Quaternion.identity) as GameObject;
             npcObject.transform.SetParent(gameObject.transform);
             npcObject.name = i+"-"+Settings.PREFAB_NPC;
