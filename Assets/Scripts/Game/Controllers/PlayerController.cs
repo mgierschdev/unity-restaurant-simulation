a590232df61dd343830abb9f948a586994e48520
pacmax2 <mgierschdev@gmail.com>
2022-08-09 11:55:43 -0400

    Adding ClickController

--- Assets/Scripts/Game/Controllers/PlayerController.cs
+++ Assets/Scripts/Game/Controllers/PlayerController.cs
@@ -1,36 +1,25 @@
 using UnityEngine;
-using System.Collections;
 using System.Collections.Generic;
 
 // Controls player properties
 // Attached to: Player Object
 public class PlayerController : GameObjectMovementBase
 {
-
-    //MovingOnLongtouch(), Long click or touch vars
-    private bool clicking;
-    private bool isLongClick;
-    private float clickingTime; // To keep the coung if longclick
-    private float longClickDuration = 0.5f; // In seconds
+    // ClickController for the long click duration for the player
+    private ClickController clickController;
 
     // Overlap spehre
-
     private void Start()
     {
         Type = ObjectType.PLAYER;
         Speed = Settings.PLAYER_MOVEMENT_SPEED;
-        // Long Click
-        clickingTime = 0;
-        clicking = false;
-        isLongClick = false;
+        GameObject cController = GameObject.FindGameObjectWithTag(Settings.CONST_PARENT_GAME_OBJECT);
+        clickController = cController.GetComponent<ClickController>();
     }
 
     // For Handling non-physics related objects
     private void Update()
     {
-        // Controls the state of the first and long click
-        ClickControl();
-
         // Player Movement on click
         if (Settings.PLAYER_WALK_ON_CLICK)
         {
@@ -60,58 +49,37 @@ public class PlayerController : GameObjectMovementBase
         UpdatePosition();
     }
 
-    private void ClickControl()
-    {
-        // first click 
-        if (Input.GetMouseButtonDown(0))
-        {
-            clickingTime = 0;
-            clicking = true;
-        }
-
-        // Resets isLongClick
-        if (isLongClick && clickingTime < longClickDuration)
-        {
-            isLongClick = false;
-        }
-    }
-
     private void MovingOnLongtouch()
     {
-        if (clicking && Input.GetKey(KeyCode.Mouse0))
+        if (clickController.IsLongClick)
         {
-            clickingTime += Time.deltaTime;
+            ResetMovementIfMoving();
 
-            if (clickingTime >= longClickDuration)
+            Vector3 mousePosition = Util.GetMouseInWorldPosition();
+            Vector3 delta = mousePosition - transform.position;
+
+            float radians = Mathf.Atan2(delta.x, delta.y);
+            float degrees = radians * Mathf.Rad2Deg;
+
+            // normalizing -180-180, 0-360
+            if (degrees < 0)
             {
-                ResetMovementIfMoving();
+                degrees += 360;
+            }
 
-                isLongClick = true;
-                Vector3 mousePosition = Util.GetMouseInWorldPosition();
-                Vector3 delta = mousePosition - transform.position;
+            AddMovement(Util.GetVectorFromDirection(Util.GetDirectionFromAngles(degrees)));
 
-                float radians = Mathf.Atan2(delta.x, delta.y);
-                float degrees = radians * Mathf.Rad2Deg;
-
-                // normalizing -180-180, 0-360
-                if (degrees < 0)
-                {
-                    degrees += 360;
-                }
-
-                AddMovement(Util.GetVectorFromDirection(Util.GetDirectionFromAngles(degrees)));
-
-                if (Settings.DEBUG_ENABLE)
-                {
-                    Debug.DrawLine(transform.position, mousePosition, Color.blue);
-                }
+            if (Settings.DEBUG_ENABLE)
+            {
+                Debug.DrawLine(transform.position, mousePosition, Color.blue);
             }
         }
+
     }
 
     private void MouseOnClick()
     {
-        if (Input.GetMouseButtonDown(0) && !isLongClick)
+        if (Input.GetMouseButtonDown(0) && !clickController.IsLongClick)
         {
             UpdatePosition();
 
