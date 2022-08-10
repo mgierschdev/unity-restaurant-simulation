121f8ba536c9a51f02f7e38ef347f7f7657c5d9d
pacmax2 <mgierschdev@gmail.com>
2022-08-10 11:41:24 -0400

    Loading tileMaps

--- Assets/Scripts/Game/Controllers/IsometricGridController.cs
+++ Assets/Scripts/Game/Controllers/IsometricGridController.cs
@@ -5,22 +5,48 @@ using System.Collections.Generic;
 // This controlls the isometric tiles on the grid
 public class IsometricGridController : MonoBehaviour
 {
-    //Tilemap
-    private Tilemap tilemap;
-    private List<GameTile> list;
-    private Dictionary<Vector3, GameTile> map;
+    //Tilemap 
+    //Floor
+    private Tilemap tilemapFloor;
+    private List<GameTile> listFloorTileMap;
+    private Dictionary<Vector3, GameTile> mapFloor;
+
+    //Colliders
+    private Tilemap tilemapColliders;
+    private List<GameTile> listCollidersTileMap;
+    private Dictionary<Vector3, GameTile> mapColliders;
+
+    //Objects
+    private Tilemap tilemapObjects;
+    private List<GameTile> listObjectsTileMap;
+    private Dictionary<Vector3, GameTile> mapObjects;
 
     void Awake()
     {
-        tilemap = GameObject.Find(Settings.TILEMAP_FLOOR_0).GetComponent<Tilemap>();
-        map = new Dictionary<Vector3, GameTile>();
-        list = new List<GameTile>();
+        tilemapFloor = GameObject.Find(Settings.TILEMAP_FLOOR_0).GetComponent<Tilemap>();
+        mapFloor = new Dictionary<Vector3, GameTile>();
+        listFloorTileMap = new List<GameTile>();
 
-        if (tilemap == null)
+        tilemapColliders = GameObject.Find(Settings.TILEMAP_COLLIDERS).GetComponent<Tilemap>();
+        mapColliders = new Dictionary<Vector3, GameTile>();
+        listCollidersTileMap = new List<GameTile>();
+
+        tilemapObjects = GameObject.Find(Settings.TILEMAP_OBJECTS).GetComponent<Tilemap>();
+        mapObjects = new Dictionary<Vector3, GameTile>();
+        listObjectsTileMap = new List<GameTile>();
+
+        if (tilemapFloor == null || tilemapColliders == null || tilemapObjects == null)
         {
             Debug.LogWarning("IsometricGridController/tilemap null");
         }
 
+        LoadTileMap(listFloorTileMap, tilemapFloor, mapFloor);
+        LoadTileMap(listCollidersTileMap, tilemapColliders, mapColliders);
+        LoadTileMap(listObjectsTileMap, tilemapObjects, mapObjects);
+    }
+
+    private void LoadTileMap(List<GameTile> list, Tilemap tilemap, Dictionary<Vector3, GameTile> map)
+    {
         foreach (Vector3 pos in tilemap.cellBounds.allPositionsWithin)
         {
             Vector3Int localPlace = Vector3Int.FloorToInt(pos);
@@ -32,8 +58,9 @@ public class IsometricGridController : MonoBehaviour
                 GameTile gameTile = new GameTile(placeInWorld, Util.GetTileType(tile.name));
                 list.Add(gameTile);
                 map.TryAdd(gameTile.GridPosition, gameTile);
-                Debug.Log("Tile " + gameTile.GridPosition + " " + gameTile.WorldPosition + " " + gameTile.Type+" ("+tile.name+")");
+                Debug.Log("Tile " + gameTile.GridPosition + " " + gameTile.WorldPosition + " " + gameTile.Type + " (" + tile.name + ")");
             }
         }
+
     }
 }
