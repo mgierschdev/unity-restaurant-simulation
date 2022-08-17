5acb3a0ab5db27620513d05900a172658615c557
pacmax2 <mgierschdev@gmail.com>
2022-08-17 16:30:45 -0400

    Adding click listener to objects in the interface

--- Assets/Scripts/Game/Controllers/ClickController.cs
+++ Assets/Scripts/Game/Controllers/ClickController.cs
@@ -8,12 +8,20 @@ public class ClickController : MonoBehaviour
     public float ClickingTime { get; set; }// To keep the coung if longclick
     private float longClickDuration = 0.2f;
 
+    private IsometricGridController gridController;
+
+    public GameObject ClickedObject { get; set; }
+
     private void Start()
     {
         // Long Click
         ClickingTime = 0;
         IsClicking = false;
         IsLongClick = false;
+
+        // Grid Controller
+        GameObject gameGridObject = gameObject.transform.Find(Settings.GAME_GRID).gameObject;
+        gridController = gameGridObject.GetComponent<IsometricGridController>();
     }
 
     private void Update()
@@ -21,7 +29,7 @@ public class ClickController : MonoBehaviour
         // Controls the state of the first and long click
         ClickControl();
 
-        //Object Cliked Control
+        //Object Cliked Control, sets the last ClickedObject
         ObjectClickedControl();
     }
 
@@ -68,14 +76,21 @@ public class ClickController : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            //Get the mouse position on the screen and send a raycast into the game world from that position.
+            Vector3Int clickPosition = gridController.GetPathFindingGridFromWorldPosition(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            GameTile tile = gridController.GetGameTileFromClickInWorldPosition(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+
+            if (tile != null)
+            {
+                Debug.Log("Tile " + tile.Type);
+            }
+            
+            Debug.Log(clickPosition);
+
             Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
-
-            //If something was hit, the RaycastHit2D.collider will not be null.
             if (hit.collider != null)
             {
-                Debug.Log(hit.collider.name);
+                ClickedObject = GameObject.Find(hit.collider.name);
             }
         }
     }
