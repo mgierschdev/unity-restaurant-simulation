ffad3fb68a71a6f972c166ca0893ddc6bfd91dc4
pacmax2 <mgierschdev@gmail.com>
2022-08-23 10:41:30 -0400

    Adding complate tables to the grid

--- Assets/Scripts/Game/Grid/GameObjectBase.cs
+++ Assets/Scripts/Game/Grid/GameObjectBase.cs
@@ -3,10 +3,18 @@ using UnityEngine.Rendering;
 
 public abstract class GameObjectBase
 {
-    protected SortingGroup sortingLayer;
-    protected IsometricGridController IsometricGrid;
+    public SortingGroup SortingLayer { get; set; }
     public Vector3Int GridPosition { get; set; } //In Pathfinding grid
     public Vector3Int LocalGridPosition { get; set; } // Local grid position, can be negatice -20,20
     public Vector3 WorldPosition { get; set; }
     public ObjectType Type { get; set; }
+
+    public void UpdateSortingLayer()
+    {
+        // Some objects may not have sorting layer defined
+        if (SortingLayer != null)
+        {
+            SortingLayer.sortingOrder = 1;
+        }
+    }
 }
