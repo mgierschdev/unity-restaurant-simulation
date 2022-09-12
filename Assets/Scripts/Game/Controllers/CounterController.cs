6c2ec1ce06c4b86d9f3e6e89d7c4426f10b09f51
pacmax2 <mgierschdev@gmail.com>
2022-09-12 16:13:34 -0400

    Adding clicklisteners inside GameGridObject

--- Assets/Scripts/Game/Controllers/CounterController.cs
+++ Assets/Scripts/Game/Controllers/CounterController.cs
@@ -5,13 +5,17 @@ public class CounterController : BaseObjectController
 
     private void Start()
     {
+        //Edit Panel Disable
+        GameObject EditPanel = transform.Find(Settings.ConstEditItemMenuPanel).gameObject;
+        EditPanel.SetActive(false);
+
         GameObject menuHandler = GameObject.Find(Settings.ConstCanvasParentMenu).gameObject;
         Util.IsNull(menuHandler, "BaseObjectController/MenuHandlerController null");
         Menu = menuHandler.GetComponent<MenuHandlerController>();
 
         GameObject gameGrid = GameObject.Find(Settings.GameGrid).gameObject;
         Grid = gameGrid.GetComponent<GridController>();
-        gameGridObject = new GameGridObject(this.transform, Grid.GetPathFindingGridFromWorldPosition(transform.position), Grid.GetLocalGridFromWorldPosition(transform.position), COST, ObjectRotation.FRONT, ObjectType.NPC_COUNTER);
+        gameGridObject = new GameGridObject(this.transform, Grid.GetPathFindingGridFromWorldPosition(transform.position), Grid.GetLocalGridFromWorldPosition(transform.position), COST, ObjectRotation.FRONT, ObjectType.NPC_COUNTER, EditPanel);
 
         if (!Util.IsNull(Grid, "CounterController/IsometricGridController null"))
         {
