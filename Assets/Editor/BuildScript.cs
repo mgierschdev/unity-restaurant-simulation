1be6a76311c9a2146cd8a156d9833d8e6573ec3e
pacmax2 <mgierschdev@gmail.com>
2022-09-16 17:46:55 -0400

    Update

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -9,53 +9,62 @@ using UnityEngine;
 // Script used for the Jenkins Build
 public class BuildScript
 {
-    public void PerformAndroidBuild()
+    static void PerformBuild()
     {
         string[] defaultScene = {
             "Assets/Scenes/World.unity",
             };
 
-        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
-        buildPlayerOptions.scenes = defaultScene;
-        buildPlayerOptions.locationPathName = "AndroidBuild" + System.DateTime.Now;
-        buildPlayerOptions.target = BuildTarget.Android;
-        buildPlayerOptions.options = BuildOptions.None;
-        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
-        
-        BuildSummary summary = report.summary;
-        if (summary.result == BuildResult.Succeeded)
-        {
-            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
-        }
-
-        if (summary.result == BuildResult.Failed)
-        {
-            Debug.Log("Build failed");
-        }
+        BuildPipeline.BuildPlayer(defaultScene, "TestBuild.apk",
+            BuildTarget.Android, BuildOptions.None);
     }
+    // static void PerformAndroidBuild()
+    // {
+    //     string[] defaultScene = {
+    //         "Assets/Scenes/World.unity",
+    //         };
 
-    public void PerformIOSBuild()
-    {
-        string[] defaultScene = {
-            "Assets/Scenes/World.unity",
-            };
+    //     BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+    //     buildPlayerOptions.scenes = defaultScene;
+    //     buildPlayerOptions.locationPathName = "AndroidBuild" + System.DateTime.Now;
+    //     buildPlayerOptions.target = BuildTarget.Android;
+    //     buildPlayerOptions.options = BuildOptions.None;
+    //     BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
 
-        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
-        buildPlayerOptions.scenes = defaultScene;
-        buildPlayerOptions.locationPathName = "iOSBuild" + System.DateTime.Now;
-        buildPlayerOptions.target = BuildTarget.iOS;
-        buildPlayerOptions.options = BuildOptions.None;
+    //     BuildSummary summary = report.summary;
+    //     if (summary.result == BuildResult.Succeeded)
+    //     {
+    //         Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+    //     }
 
-        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
-        BuildSummary summary = report.summary;
-        if (summary.result == BuildResult.Succeeded)
-        {
-            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
-        }
+    //     if (summary.result == BuildResult.Failed)
+    //     {
+    //         Debug.Log("Build failed");
+    //     }
+    // }
 
-        if (summary.result == BuildResult.Failed)
-        {
-            Debug.Log("Build failed");
-        }
-    }
+    // statics void PerformIOSBuild()
+    // {
+    //     string[] defaultScene = {
+    //         "Assets/Scenes/World.unity",
+    //         };
+
+    //     BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+    //     buildPlayerOptions.scenes = defaultScene;
+    //     buildPlayerOptions.locationPathName = "iOSBuild" + System.DateTime.Now;
+    //     buildPlayerOptions.target = BuildTarget.iOS;
+    //     buildPlayerOptions.options = BuildOptions.None;
+
+    //     BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+    //     BuildSummary summary = report.summary;
+    //     if (summary.result == BuildResult.Succeeded)
+    //     {
+    //         Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+    //     }
+
+    //     if (summary.result == BuildResult.Failed)
+    //     {
+    //         Debug.Log("Build failed");
+    //     }
+    // }
 }
