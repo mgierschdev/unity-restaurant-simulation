97b2d666fc3b211f5c92bc17c6655cfaf989aab0
pacmax2 <mgierschdev@gmail.com>
2022-09-26 16:56:53 -0400

    Adding waiting animation to the code

--- Assets/Scripts/Game/Players/AnimatorState.cs
+++ Assets/Scripts/Game/Players/AnimatorState.cs
@@ -2,4 +2,5 @@ public static class AnimatorState
 {
     public const string Walking = "Walking";
     public const string Idle = "Idle";
+    public const string WaitingAtTable = "WaitingAtTable";
 }
