1537c5a2bb9c71d9e66a465d7e0e9f3995ffbf4a
pacmax2 <mgierschdev@gmail.com>
2022-09-27 14:22:52 -0400

    Refactoring GameGridObject to accept rotation for all objects

--- Assets/Scripts/Game/Players/MenuObjectList.cs
+++ Assets/Scripts/Game/Players/MenuObjectList.cs
@@ -15,24 +15,25 @@ public class MenuObjectList
         StoreItemDictionary = new Dictionary<string, StoreGameObject>();
         StoreItemTypeDic = new Dictionary<StoreItemType, StoreGameObject>();
 
-        AllStoreItems.Add(new StoreGameObject("Wooden table", "SingleTable-1", ObjectType.NPC_SINGLE_TABLE, StoreItemType.WOODEN_TABLE_SINGLE, 20));
-        AllStoreItems.Add(new StoreGameObject("Wooden squared table", "SingleTable-2", ObjectType.NPC_SINGLE_TABLE, StoreItemType.SQUARED_WOODEN_TABLE_SINGLE, 40));
-        AllStoreItems.Add(new StoreGameObject("Wooden table", "SingleTable-3", ObjectType.NPC_SINGLE_TABLE, StoreItemType.TABLE_SINGLE_3, 50));
-        AllStoreItems.Add(new StoreGameObject("Dark wood table", "SingleTable-4", ObjectType.NPC_SINGLE_TABLE, StoreItemType.TABLE_SINGLE_4, 60));
-        AllStoreItems.Add(new StoreGameObject("Wooden table", "SingleTable-5", ObjectType.NPC_SINGLE_TABLE, StoreItemType.TABLE_SINGLE_5, 70));
-        AllStoreItems.Add(new StoreGameObject("Dark wood table", "SingleTable-6", ObjectType.NPC_SINGLE_TABLE, StoreItemType.TABLE_SINGLE_6, 80));
-        AllStoreItems.Add(new StoreGameObject("Wooden table", "SingleTable-7", ObjectType.NPC_SINGLE_TABLE, StoreItemType.TABLE_SINGLE_7, 90));
-        AllStoreItems.Add(new StoreGameObject("Dark wood table", "SingleTable-8", ObjectType.NPC_SINGLE_TABLE, StoreItemType.TABLE_SINGLE_8, 100));
-        AllStoreItems.Add(new StoreGameObject("Iron table", "SingleTable-9", ObjectType.NPC_SINGLE_TABLE, StoreItemType.TABLE_SINGLE_9, 200));
-        AllStoreItems.Add(new StoreGameObject("Iron table", "SingleTable-10", ObjectType.NPC_SINGLE_TABLE, StoreItemType.TABLE_SINGLE_10, 200));
-        
-        AllStoreItems.Add(new StoreGameObject("Counter", "Counter-1", ObjectType.NPC_COUNTER, StoreItemType.COUNTER, 999999));
-        
-        AllStoreItems.Add(new StoreGameObject("Wooden container", "BaseContainer-1", ObjectType.SINGLE_CONTAINER, StoreItemType.WOODEN_BASE_CONTAINER, 40));
-        
-        AllStoreItems.Add(new StoreGameObject("UNDEFINED", "UNDEFINED", ObjectType.UNDEFINED, StoreItemType.UNDEFINED, 999999));
+        AllStoreItems.Add(new StoreGameObject("Wooden table", "SingleTable-1", ObjectType.NPC_SINGLE_TABLE, StoreItemType.WOODEN_TABLE_SINGLE, Settings.SpriteLibCategoryTables, 20));
+        AllStoreItems.Add(new StoreGameObject("Wooden squared table", "SingleTable-2", ObjectType.NPC_SINGLE_TABLE, StoreItemType.SQUARED_WOODEN_TABLE_SINGLE, Settings.SpriteLibCategoryTables, 40));
+        AllStoreItems.Add(new StoreGameObject("Wooden table", "SingleTable-3", ObjectType.NPC_SINGLE_TABLE, StoreItemType.TABLE_SINGLE_3, Settings.SpriteLibCategoryTables, 50));
+        AllStoreItems.Add(new StoreGameObject("Dark wood table", "SingleTable-4", ObjectType.NPC_SINGLE_TABLE, StoreItemType.TABLE_SINGLE_4, Settings.SpriteLibCategoryTables, 60));
+        AllStoreItems.Add(new StoreGameObject("Wooden table", "SingleTable-5", ObjectType.NPC_SINGLE_TABLE, StoreItemType.TABLE_SINGLE_5, Settings.SpriteLibCategoryTables, 70));
+        AllStoreItems.Add(new StoreGameObject("Dark wood table", "SingleTable-6", ObjectType.NPC_SINGLE_TABLE, StoreItemType.TABLE_SINGLE_6, Settings.SpriteLibCategoryTables, 80));
+        AllStoreItems.Add(new StoreGameObject("Wooden table", "SingleTable-7", ObjectType.NPC_SINGLE_TABLE, StoreItemType.TABLE_SINGLE_7, Settings.SpriteLibCategoryTables, 90));
+        AllStoreItems.Add(new StoreGameObject("Dark wood table", "SingleTable-8", ObjectType.NPC_SINGLE_TABLE, StoreItemType.TABLE_SINGLE_8, Settings.SpriteLibCategoryTables, 100));
+        AllStoreItems.Add(new StoreGameObject("Iron table", "SingleTable-9", ObjectType.NPC_SINGLE_TABLE, StoreItemType.TABLE_SINGLE_9, Settings.SpriteLibCategoryTables, 200));
+        AllStoreItems.Add(new StoreGameObject("Iron table", "SingleTable-10", ObjectType.NPC_SINGLE_TABLE, StoreItemType.TABLE_SINGLE_10, Settings.SpriteLibCategoryTables, 200));
 
-        foreach(StoreGameObject storeItem in AllStoreItems){
+        AllStoreItems.Add(new StoreGameObject("Counter", "Counter-1", ObjectType.NPC_COUNTER, StoreItemType.COUNTER, Settings.SpriteLibCategoryStoreObjects, 999999));
+
+        AllStoreItems.Add(new StoreGameObject("Wooden container", "BaseContainer-1", ObjectType.SINGLE_CONTAINER, StoreItemType.WOODEN_BASE_CONTAINER, Settings.SpriteLibCategoryContainers, 40));
+
+        AllStoreItems.Add(new StoreGameObject("UNDEFINED", "UNDEFINED", ObjectType.UNDEFINED, StoreItemType.UNDEFINED, "UNDEFINED", 999999));
+
+        foreach (StoreGameObject storeItem in AllStoreItems)
+        {
             StoreItemDictionary.Add(storeItem.Identifier, storeItem);
             StoreItemTypeDic.Add(storeItem.StoreItemType, storeItem);
         }
--- Assets/Scripts/Game/Players/StoreGameObject.cs
+++ Assets/Scripts/Game/Players/StoreGameObject.cs
@@ -6,14 +6,15 @@ public class StoreGameObject
     public ObjectType Type { get; set; }
     public StoreItemType StoreItemType { get; set; }
     public string MenuItemSprite { get; set; }
+    public string SpriteLibCategory { get; set; }
 
-    public StoreGameObject(string name, string identifier, ObjectType type, StoreItemType tableType, int cost)
+    public StoreGameObject(string name, string identifier, ObjectType type, StoreItemType tableType, string CategorySprite, int cost)
     {
         Identifier = identifier;
         Name = name;
         Cost = cost;
         Type = type;
         StoreItemType = tableType;
-        MenuItemSprite = Settings.StoreSpritePath+identifier;
+        MenuItemSprite = Settings.StoreSpritePath + identifier;
     }
 }
