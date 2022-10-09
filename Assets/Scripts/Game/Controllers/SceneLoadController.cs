6979c5c790defe95dc0b5e47bd1a95c11749e90e
pacmax2 <mgierschdev@gmail.com>
2022-10-09 11:24:22 -0400

    Adding slider logic

--- Assets/Scripts/Game/Controllers/SceneLoadController.cs
+++ Assets/Scripts/Game/Controllers/SceneLoadController.cs
@@ -1,20 +1,50 @@
 using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 // This controller is attached to the -LoadScene-  not to the main game scene -World-
 // Check the folder under Assets/Scenes
 public class SceneLoadController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private TextMeshProUGUI sliderProgress;
+    private Slider slider;
+
     void Start()
     {
-        
+        //We get the slider 
+        GameObject sliderGameObject = GameObject.FindGameObjectWithTag(Settings.SliderTag);
+        slider = sliderGameObject.GetComponent<Slider>();
+        Util.IsNull(sliderGameObject, "SceneLoadController/Start Slider is null");
+
+        //We get the text inside the slider
+        GameObject sliderProgressObject = sliderGameObject.transform.Find(Settings.SliderProgress).gameObject;
+        Util.IsNull(sliderProgressObject, "SceneLoadController/Start sliderProgressObject is null");
+        sliderProgress = sliderProgressObject.GetComponent<TextMeshProUGUI>();
+
+        //Default values
+        slider.maxValue = 1;
+        slider.value = 0;
+
+        //We load the Scene managment 
+        LoadSceneAsync(Settings.GameScene);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LoadSceneAsync(string scene)
     {
-        
+        StartCoroutine(LoadAsync(scene));
+    }
+
+    private IEnumerator LoadAsync(string scene)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(scene); // LoadSceneMode.Additive, default Single
+
+        while (!operation.isDone)
+        {
+            slider.value = operation.progress;
+            sliderProgress.text = (operation.progress * 100) + " % ";
+            yield return null;
+        }
     }
 }
