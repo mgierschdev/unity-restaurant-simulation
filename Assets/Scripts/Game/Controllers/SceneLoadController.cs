b6179fa1c79e6f5ea8e21c973e0b554210bf628c
pacmax2 <mgierschdev@gmail.com>
2022-10-09 10:24:21 -0400

    Moving all the scenes into Assets/Scenes/, Adding SceneLoadController, Re-namming Auth to AuthController, Expanding main World scene UI to fit the current screen size, Creating LoadScene and adding Scene Loading UI

---
+++ Assets/Scripts/Game/Controllers/SceneLoadController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This controller is attached to the -LoadScene-  not to the main game scene -World-
+// Check the folder under Assets/Scenes
+public class SceneLoadController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
