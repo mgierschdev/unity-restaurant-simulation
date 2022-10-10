71aa679b975330aea484a8f6fca796128ad9c1e2
pacmax2 <mgierschdev@gmail.com>
2022-10-10 18:20:41 -0400

    Updating auth controller

--- Assets/Scripts/Game/Controllers/AuthController.cs
+++ Assets/Scripts/Game/Controllers/AuthController.cs
@@ -4,49 +4,11 @@ using UnityEngine;
 
 public class AuthController : MonoBehaviour
 {
-    // public void Start()
-    // {
-    //     // LoadAuth();
-    // }
-
-    // void InitializePlayGamesLogin()
-    // {
-    //     var config = new PlayGamesClientConfiguration.Builder()
-    //         // Requests an ID token be generated.  
-    //         // This OAuth token can be used to
-    //         // identify the player to other services such as Firebase.
-    //         .RequestIdToken()
-    //         .Build();
-
-    //     PlayGamesPlatform.InitializeInstance(config);
-    //     PlayGamesPlatform.DebugLogEnabled = true;
-    //     PlayGamesPlatform.Activate();
-    // }
-
-    // void LoginGoogle()
-    // {
-    //     //Social.localUser.Authenticate(ProcessAuthentication);
-    // }
-
-    // void OnGooglePlayGamesLogin(bool success)
-    // {
-    //     if (success)
-    //     {
-    //         // Call Unity Authentication SDK to sign in or link with Google.
-    //         Debug.Log("Login with Google done. IdToken: " + ((PlayGamesLocalUser)Social.localUser).GetIdToken());
-    //     }
-    //     else
-    //     {
-    //         Debug.Log("Unsuccessful login");
-    //     }
-    // }
-
     public void LoadAuth()
     {
         PlayGamesPlatform.DebugLogEnabled = true;
         PlayGamesPlatform.Activate();
         PlayGamesPlatform.Instance.Authenticate(ProcessAuthentication);
-        //PlayGamesPlatform.Instance.ManuallyAuthenticate(ProcessAuthentication);
     }
 
     internal void ProcessAuthentication(SignInStatus status)
