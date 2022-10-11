067fa199fb50d7e50876ee812d126318d28729b9
pacmax2 <mgierschdev@gmail.com>
2022-10-11 12:01:18 -0400

    Allowing only landscape adding simple auth

--- Assets/Scripts/Game/Controllers/AuthController.cs
+++ Assets/Scripts/Game/Controllers/AuthController.cs
@@ -1,27 +1,27 @@
-// using GooglePlayGames;
-// using GooglePlayGames.BasicApi;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
 using UnityEngine;
 
 public class AuthController : MonoBehaviour
 {
-    // public void LoadAuth()
-    // {
-    //     PlayGamesPlatform.DebugLogEnabled = true;
-    //     PlayGamesPlatform.Activate();
-    //     PlayGamesPlatform.Instance.Authenticate(ProcessAuthentication);
-    // }
+    public void LoadAuth()
+    {
+        PlayGamesPlatform.DebugLogEnabled = true;
+        PlayGamesPlatform.Activate();
+        PlayGamesPlatform.Instance.Authenticate(ProcessAuthentication);
+    }
 
-    // internal void ProcessAuthentication(SignInStatus status)
-    // {
-    //     if (status == SignInStatus.Success)
-    //     {
-    //         // Continue with Play Games Services
-    //         Debug.Log("UNITY: Authenticated." + Social.localUser.userName + " (" + Social.localUser.id);
-    //         PlayGamesPlatform.Instance.ShowAchievementsUI();
-    //     }
-    //     else
-    //     {
-    //         Debug.Log("UNITY: Failed to authenticate " + status);
-    //     }
-    // }
+    internal void ProcessAuthentication(SignInStatus status)
+    {
+        if (status == SignInStatus.Success)
+        {
+            // Continue with Play Games Services
+            Debug.Log("UNITY: Authenticated." + Social.localUser.userName + " (" + Social.localUser.id);
+            PlayGamesPlatform.Instance.ShowAchievementsUI();
+        }
+        else
+        {
+            Debug.Log("UNITY: Failed to authenticate " + status);
+        }
+    }
 }
