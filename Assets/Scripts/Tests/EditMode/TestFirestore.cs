1e599b9e9ca6db3199083aee1a3982d8b2dba913
pacmax2 <mgierschdev@gmail.com>
2022-10-11 17:17:49 -0400

    Adding insert, update tests

--- Assets/Scripts/Tests/EditMode/TestFirestore.cs
+++ Assets/Scripts/Tests/EditMode/TestFirestore.cs
@@ -3,10 +3,11 @@ using Firebase.Firestore;
 using NUnit.Framework;
 using UnityEngine;
 
+//This test only passes if the firestore emulator suite is running locally and firestore running at 8080
 public class TestFirestore
 {
     private const string DEV_HOST = "localhost:8080";
-    private const string TEST_COLLECTION = "TestUsers";
+    private const string TEST_COLLECTION = "Test";
     private Firestore test;
     private MockUser user;
 
@@ -27,30 +28,38 @@ public class TestFirestore
         { "nullExample", null },
         { "arrayExample", new List<object>() { 5, true, "Hello" } },
         { "objectExample", new Dictionary<string, object>
-                {
-                        { "a", 5 },
-                        { "b", true },
-                }
+            {
+                { "a", 5 },
+                { "b", true },
+            }
         },};
+
         FirebaseFirestore firestore = FirebaseFirestore.DefaultInstance;
         firestore.Settings.Host = DEV_HOST;
         firestore.Settings.SslEnabled = false;
-        await firestore.Collection(TEST_COLLECTION).Document("User").SetAsync(docData);
+
+        DocumentReference dataTypesReference = firestore.Collection(TEST_COLLECTION).Document("Datatypes");
+        DocumentReference usersReference = firestore.Collection(TEST_COLLECTION).Document(user.ID);
+
+        await dataTypesReference.SetAsync(docData);
+        await usersReference.SetAsync(user.GetUserAsMap());
+
+        DocumentSnapshot snapshot = await usersReference.GetSnapshotAsync();
+        Debug.Log("snapshot 1 "+snapshot.Id);
+        if (snapshot.Exists)
+        {
+            Assert.AreEqual(snapshot.Id, user.ID);
+        }
+        else
+        {
+            throw new FirestoreException(FirestoreError.NotFound);//not found
+        }
+
+        // await usersReference.DeleteAsync();
+        // await dataTypesReference.DeleteAsync();
+        // DocumentSnapshot snapshot2 = await usersReference.GetSnapshotAsync();
+        // Assert.False(snapshot2.Exists);
+        // DocumentSnapshot snapshot3 = await dataTypesReference.GetSnapshotAsync();
+        // Assert.False(snapshot3.Exists);
     }
-
-    [Test]
-    public async void TestSavingMockUser()
-    {
-        FirebaseFirestore firestore = FirebaseFirestore.DefaultInstance;
-        Debug.Log(user.ID);
-        firestore.Settings.Host = DEV_HOST;
-        firestore.Settings.SslEnabled = false;
-        await firestore.Collection(TEST_COLLECTION).Document(user.ID).SetAsync(user.GetUserAsMap());
-    }
-
-    // [Test]
-    // public async void TestReadMockUser()
-    // {
-
-    // }
 }
