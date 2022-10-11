126a34bebb736c7a0e4385359d01fbee6327aed7
pacmax2 <mgierschdev@gmail.com>
2022-10-11 16:10:59 -0400

    Writing emulator test working

--- Assets/Scripts/Services/Firebase/Firestore.cs
+++ Assets/Scripts/Services/Firebase/Firestore.cs
@@ -1,6 +1,6 @@
 using System;
-using System.Linq;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;  // Needed for the Unwrap extension method
 using Firebase;
 using Firebase.Extensions;
@@ -11,7 +11,7 @@ using DependencyStatus = Firebase.DependencyStatus;
 public class Firestore
 {
     private bool isFirebaseEnabled;
-    private LogLevel logLevel = LogLevel.Info;
+    private LogLevel logLevel = LogLevel.Verbose;
     private FirebaseFirestore firestore;
     private DocumentReference docReference;
     private string collectionName;
@@ -28,9 +28,23 @@ public class Firestore
 
     public Firestore(string devHost, string collectionName, string document)
     {
-        firestore = FirebaseFirestore.DefaultInstance;
-        firestore.Settings.SslEnabled = true;
-        firestore.Settings.Host = devHost;
+        AppOptions options = new AppOptions
+        {
+            ApiKey = "<API_KEY>",
+            AppId = "<GOOGLE_APP_ID>",
+            ProjectId = "fir-unitygamebussbackend"
+        };
+        // app.Options.ProjectId = "fir-unitygamebussbackend";
+        // app.Options.DatabaseUrl = new Uri("http://localhost:8080/");
+        FirebaseApp app = FirebaseApp.Create(options, "Secondary");
+        if(app == null){
+            Debug.Log("FirebaseApp null");
+        }
+
+        firestore = FirebaseFirestore.GetInstance(app);
+        Debug.Log(firestore.App+" "+firestore.ToString() +" "+ app.ToString());
+        firestore.Settings.SslEnabled = false;
+        firestore.Settings.Host = "https://localhost:8080";
         this.collectionName = collectionName;
         this.document = document;
         docReference = firestore.Collection(collectionName).Document(document);
@@ -40,42 +54,47 @@ public class Firestore
     {
         Debug.Log("Saving dic ");
 
-        return docReference.SetAsync(dictionary).ContinueWithOnMainThread(task =>
+        try
         {
-            Debug.Log("Data added");
-        });
+            return docReference.SetAsync(dictionary);
+        }
+        catch (FirebaseException e)
+        {
+            GameLog.LogError("Exception raised: Firestore/SaveDictionary " + e.ToString());
+            return null;
+        }
     }
 
     public Task InitFirebase()
     {
         return FirebaseApp.CheckDependenciesAsync().ContinueWith(checkTask =>
-         {
-             DependencyStatus status = checkTask.Result;
-             if (status != DependencyStatus.Available)
-             {
-                 return FirebaseApp.FixDependenciesAsync().ContinueWith(t =>
-                 {
-                     return FirebaseApp.CheckDependenciesAsync();
-                 }).Unwrap();
-             }
-             else
-             {
-                 return checkTask;
-             }
-         }).Unwrap().ContinueWith(task =>
-         {
-             DependencyStatus dependencyStatus = task.Result;
-             if (dependencyStatus == DependencyStatus.Available)
-             {
-                 GameLog.Log("Firebase is loaded");
-                 isFirebaseEnabled = true;
-             }
-             else
-             {
-                 GameLog.LogError("Error: Could not resolve all Firebase dependencies: " + dependencyStatus);
-                 isFirebaseEnabled = false;
-             }
-         });
+        {
+            DependencyStatus status = checkTask.Result;
+            if (status != DependencyStatus.Available)
+            {
+                return FirebaseApp.FixDependenciesAsync().ContinueWith(t =>
+                {
+                    return FirebaseApp.CheckDependenciesAsync();
+                }).Unwrap();
+            }
+            else
+            {
+                return checkTask;
+            }
+        }).Unwrap().ContinueWith(task =>
+        {
+            DependencyStatus dependencyStatus = task.Result;
+            if (dependencyStatus == DependencyStatus.Available)
+            {
+                GameLog.Log("Firebase is loaded");
+                isFirebaseEnabled = true;
+            }
+            else
+            {
+                GameLog.LogError("Error: Could not resolve all Firebase dependencies: " + dependencyStatus);
+                isFirebaseEnabled = false;
+            }
+        });
     }
 
     public bool GetIsFirebaseEnabled()
--- Assets/Scripts/Util/Data/MockUser.cs
+++ Assets/Scripts/Util/Data/MockUser.cs
@@ -1,24 +1,34 @@
 using System;
+using System.Collections.Generic;
 
 // Mock User to use agains the database
 public class MockUser
 {
     public string FirstName = "First Name";
     public string SecondName = "Second Name";
-    public string ID = "3ASOD139FB10V1AS";
+    public string ID;
     public string Auth = "google play";
     public DateTime LastLogin;
     public DateTime SignInDate;
 
     public MockUser()
     {
-        this.SignInDate = new DateTime(2022, 0, 0, 0, 0, 0);
-        this.LastLogin = new DateTime(2022, 0, 0, 0, 0, 1);
+        this.SignInDate = DateTime.Now;
+        this.LastLogin = DateTime.Now;
         ID = GenerateID();
     }
 
     private string GenerateID()
     {
         return Guid.NewGuid().ToString();
+    }
+
+    public Dictionary<string, object> GetUserAsMap(){
+        return new Dictionary<string, object>{
+            {"Name", new List<object>(){FirstName, SecondName}},
+            {"Auth", Auth},
+            {"LastLogin", LastLogin},
+            {"CreatedAt", SignInDate}
+        };
     }
 }
