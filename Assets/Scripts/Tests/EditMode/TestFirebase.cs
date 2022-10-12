9bcc2014df33eef4b0e47838f8161dcc4ffcd708
pacmax2 <mgierschdev@gmail.com>
2022-10-12 10:48:51 -0400

    Adding test for the cloud function hello world

--- Assets/Scripts/Tests/EditMode/TestFirebase.cs
+++ Assets/Scripts/Tests/EditMode/TestFirebase.cs
@@ -1,14 +1,16 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Firebase.Extensions;
 using Firebase.Firestore;
-using Firebase.Auth;
+using Firebase.Functions;
 using NUnit.Framework;
 using UnityEngine;
 
 //This test only passes if the firestore emulator suite is running locally and firestore running at 8080
-public class TestFirestore
+public class TestFirebase
 {
-    private const string DEV_HOST = "localhost:8080";
+    private const string DEV_FIRESTORE_HOST = "localhost:8080";
+    private const string DEV_CLOUD_FUNCTION_HOST = "localhost:5001";
     private const string TEST_COLLECTION = "Test";
     private FirebaseFirestore firestore;
     private MockUser user;
@@ -39,9 +41,9 @@ public class TestFirestore
         firestore = FirebaseFirestore.DefaultInstance;
         // firestore.Settings.Host is cached beteween tests 
         Debug.Log("Current firestore host " + firestore.Settings.Host);
-        if (!firestore.Settings.Host.Contains(DEV_HOST))
+        if (!firestore.Settings.Host.Contains(DEV_FIRESTORE_HOST))
         {
-            firestore.Settings.Host = DEV_HOST;
+            firestore.Settings.Host = DEV_FIRESTORE_HOST;
             firestore.Settings.SslEnabled = false;
         }
 
@@ -69,13 +71,46 @@ public class TestFirestore
         }).GetAwaiter();
     }
 
+    [Test]
+    public void TestCloudFunction()
+    {
+        FirebaseFunctions functions = FirebaseFunctions.GetInstance(DEV_CLOUD_FUNCTION_HOST);
+        HttpsCallableReference function = functions.GetHttpsCallable("helloWorld");
+        string functionInput = "functionInput";
+
+        function.CallAsync(functionInput).ContinueWithOnMainThread((response) =>
+       {
+           Debug.Log("response = " + response.Result.Data.ToString());
+
+           if (response.IsFaulted || response.IsCanceled)
+           {
+               Firebase.FirebaseException e = response.Exception.Flatten().InnerExceptions[0] as Firebase.FirebaseException;
+               FunctionsErrorCode error = (FunctionsErrorCode)e.ErrorCode;
+
+               Debug.LogError("Fault!");
+               Debug.Log("FunctionsErrorCode! = " + error);
+           }
+           else
+           {
+            //    string returnedName = response.Result.Data.ToString();
+            //    if (returnedName == functionInput)
+            //    {
+            //        //Name already exists in database
+            //    }
+            //    else if (string.IsNullOrEmpty(returnedName))
+            //    {
+            //        //Name doesn't exist in database
+            //    }
+           }
+       });
+    }
+
+    // Waiting for future support
     // [Test]
     // public void TestFiresbaseAuth()
     // {
-   
     //     // //app.Options.DatabaseUrl = new System.Uri("localhost:9099");
     //     // FirebaseAuth auth = FirebaseAuth.GE
-
     //     // // Debug.Log(" "+auth.SignInWithCredentialAsync());
     //     // Task.Run(() => auth.SignInAnonymouslyAsync());
     //     // Debug.Log(auth.CurrentUser);
