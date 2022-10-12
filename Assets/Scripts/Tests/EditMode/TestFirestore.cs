c63ae2a668d21b162a40d09c9e23b2c18961b325
pacmax2 <mgierschdev@gmail.com>
2022-10-12 10:00:31 -0400

    Removing empty firestore auth, adding firebase app assembly, using Task.run with awaiter to run firestore async, checking if firestore is being cache between calls

--- Assets/Scripts/Tests/EditMode/TestFirestore.cs
+++ Assets/Scripts/Tests/EditMode/TestFirestore.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Firebase.Firestore;
 using NUnit.Framework;
 using UnityEngine;
@@ -8,7 +9,7 @@ public class TestFirestore
 {
     private const string DEV_HOST = "localhost:8080";
     private const string TEST_COLLECTION = "Test";
-    private Firestore test;
+    private FirebaseFirestore firestore;
     private MockUser user;
 
     [SetUp]
@@ -18,7 +19,7 @@ public class TestFirestore
     }
 
     [Test]
-    public async void TestSavingData()
+    public void TestSavingData()
     {
         Dictionary<string, object> docData = new Dictionary<string, object>
         {
@@ -34,32 +35,58 @@ public class TestFirestore
             }
         },};
 
-        FirebaseFirestore firestore = FirebaseFirestore.DefaultInstance;
-        firestore.Settings.Host = DEV_HOST;
-        firestore.Settings.SslEnabled = false;
+        firestore = FirebaseFirestore.DefaultInstance;
+        // firestore.Settings.Host is cached beteween tests 
+        Debug.Log("Current firestore host " + firestore.Settings.Host);
+        if (!firestore.Settings.Host.Contains(DEV_HOST))
+        {
+            firestore.Settings.Host = DEV_HOST;
+            firestore.Settings.SslEnabled = false;
+        }
 
         DocumentReference dataTypesReference = firestore.Collection(TEST_COLLECTION).Document("Datatypes");
         DocumentReference usersReference = firestore.Collection(TEST_COLLECTION).Document(user.ID);
 
-        await dataTypesReference.SetAsync(docData);
-        await usersReference.SetAsync(user.GetUserAsMap());
+        Task.Run(() => dataTypesReference.SetAsync(docData)).GetAwaiter();
+        Task.Run(() => usersReference.SetAsync(user.GetUserAsMap())).GetAwaiter();
 
-        DocumentSnapshot snapshot = await usersReference.GetSnapshotAsync();
-        Debug.Log("snapshot 1 "+snapshot.Id);
-        if (snapshot.Exists)
+        DocumentSnapshot snapshot = null;
+        Task.Run(async () =>
         {
+            snapshot = await usersReference.GetSnapshotAsync();
+            Debug.Log("snapshot1 ID: " + snapshot.Id);
             Assert.AreEqual(snapshot.Id, user.ID);
-        }
-        else
-        {
-            throw new FirestoreException(FirestoreError.NotFound);//not found
-        }
+        }).GetAwaiter();
 
-        // await usersReference.DeleteAsync();
-        // await dataTypesReference.DeleteAsync();
-        // DocumentSnapshot snapshot2 = await usersReference.GetSnapshotAsync();
-        // Assert.False(snapshot2.Exists);
-        // DocumentSnapshot snapshot3 = await dataTypesReference.GetSnapshotAsync();
-        // Assert.False(snapshot3.Exists);
+        usersReference.DeleteAsync().GetAwaiter();
+
+        Task.Run(async () =>
+        {
+            snapshot = await usersReference.GetSnapshotAsync();
+            Debug.Log("Assert false "+snapshot.Exists);
+            Assert.False(snapshot.Exists);
+        }).GetAwaiter();
     }
 }
+
+// private Task InitAuthAnonymosly()
+// {
+//     auth = FirebaseAuth.DefaultInstance;
+
+//     return auth.SignInAnonymouslyAsync().ContinueWith(task =>
+//     {
+//         if (task.IsCanceled)
+//         {
+//             Debug.LogError("SignInAnonymouslyAsync was canceled.");
+//             return;
+//         }
+//         if (task.IsFaulted)
+//         {
+//             Debug.LogError("SignInAnonymouslyAsync encountered an error: " + task.Exception);
+//             return;
+//         }
+//         isUserSignedIn = true;
+//         FirebaseUser newUser = task.Result;
+//         Debug.LogFormat("User signed in successfully: {0} ({1})", newUser.DisplayName, newUser.UserId);
+//     });
+// }
