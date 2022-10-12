31223b793492ea8da4d0377fd6f3cf3355131950
pacmax2 <mgierschdev@gmail.com>
2022-10-12 13:07:33 -0400

    Merging Mockup user to player data and refactoring test, also removing tmp button from the left bottom panel

--- Assets/Scripts/Game/Players/PlayerData.cs
+++ Assets/Scripts/Game/Players/PlayerData.cs
@@ -1,15 +1,26 @@
+using System;
 using System.Collections.Generic;
 using TMPro;
 
 public class PlayerData
 {
+    public string FirstName = "undefined"; // optional
+    public string SecondName = "undefined"; // optional
+    public string EmailID = "undefined@gmail.com"; // mandatory
+    public string InternalID; // internal app id
+    public string FireappAuthID = "undefined";
+    public string Auth = "undefined"; // TODO: Type of auth, we can serialized to an enum
+    public string LanguageCode = "undefined"; // In the standard format (Locale) en_US, es_ES 
+    public DateTime LastLogin;
+    public DateTime SignInDate;
+
     private double money;
     private TextMeshProUGUI moneyText;
     private List<GameGridObject> storedIventory;
     private List<GameGridObject> Inventory;
     private HashSet<string> setStoredInventory; // Saved stored inventory by ID
 
-    public PlayerData(double money, TextMeshProUGUI moneyText)
+    public PlayerData(double money, TextMeshProUGUI moneyText) // Recieves the reference to the UI Text
     {
         this.money = money;
         this.moneyText = moneyText;
@@ -17,6 +28,10 @@ public class PlayerData
         Inventory = new List<GameGridObject>();
         storedIventory = new List<GameGridObject>();
         setStoredInventory = new HashSet<string>();
+    }
+
+    public PlayerData(){
+
     }
 
     public void AddMoney(double amount)
@@ -64,5 +79,28 @@ public class PlayerData
     public void AddItemToInventory(GameGridObject obj)
     {
         Inventory.Add(obj);
+    }
+
+    private string GenerateID()
+    {
+        return Guid.NewGuid().ToString();
+    }
+
+    public Dictionary<string, object> GetMockUserAsMap()
+    {
+        this.SignInDate = DateTime.Now;
+        this.LastLogin = DateTime.Now;
+        InternalID = GenerateID();
+        EmailID = InternalID +"@gmail.com";
+
+        return new Dictionary<string, object>{
+            {"Name", new List<object>(){FirstName, SecondName}},
+            {"LanguageCode", LanguageCode},
+            {"ID", InternalID},
+            {"FireappAuthID", FireappAuthID},
+            {"Auth", Auth},
+            {"LastLogin", LastLogin},
+            {"CreatedAt", SignInDate}
+        };
     }
 }
