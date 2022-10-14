3cf7e513407485153722804e7799472a7b13fa4f
pacmax2 <mgierschdev@gmail.com>
2022-10-14 12:27:16 -0400

    Adding Level Calculator, Unit tests, Setting the level text and the gems text

---
+++ Assets/Scripts/Game/Players/PlayerLevelCalculator.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+
+// calculates the player level based on the experience
+// Also gives the amount of experience per money or gems spent
+public static class PlayerLevelCalculator
+{
+    private static List<Double> ExpLevelMap;
+    private static int MAX_LEVEL = 100;
+
+    private static void Init()
+    {
+        if (ExpLevelMap == null)
+        {
+            ExpLevelMap = new List<Double>();
+
+            for (int i = 0; i <= 100; i++)
+            {
+                ExpLevelMap.Add(GetExpToLevel(i));//();
+                GameLog.Log(ExpLevelMap[i].ToString() + " " + i);//prints current level
+            }
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    public static Double GetExperienceFromGemsSpent(Double amount)
+    {
+        return amount is < 0 or >= (Double.MaxValue / 8) ? 0 : amount * 8;
+    }
+
+    public static Double GetExperienceFromMoneySpent(Double amount)
+    {
+        return amount is < 0 or >= (Double.MaxValue / 8) ? 0 : amount * 2;
+    }
+
+    public static Double GetExperienceToNextLevel(Double experience)
+    {
+        if (experience is < 0 or >= Double.MaxValue)
+        {
+            return 0;
+        }
+
+        Init();
+        int index = ExpLevelMap.BinarySearch(experience);
+        index = Math.Abs(index);
+        return index == 0 ? ExpLevelMap[0] - experience : ExpLevelMap[index - 1] - experience;// to be checked
+    }
+
+    public static Double GetExpToLevel(int level)
+    {
+        if (level < 0)
+        {
+            return 0;
+        }
+
+        if (level > MAX_LEVEL)
+        {
+            level = MAX_LEVEL;
+        }
+        return MathF.Pow(level, 3) + level * 2 + 20;
+    }
+}
---
+++ Assets/Scripts/Tests/EditMode/TestExpLevelCalculator.cs
@@ -0,0 +1,50 @@
+using System;
+using NUnit.Framework;
+
+public class TestExpLevelCalculator
+{
+    [Test]
+    public void TestGetExperienceFromGems()
+    {
+        Assert.AreEqual(PlayerLevelCalculator.GetExperienceFromGemsSpent(0), 0.0);
+        Assert.AreEqual(PlayerLevelCalculator.GetExperienceFromGemsSpent(-1), 0.0);
+        Assert.AreEqual(PlayerLevelCalculator.GetExperienceFromGemsSpent(Double.MinValue), 0.0);
+        Assert.AreEqual(PlayerLevelCalculator.GetExperienceFromGemsSpent(Double.MaxValue), 0);
+        Assert.AreEqual(PlayerLevelCalculator.GetExperienceFromGemsSpent(Double.MaxValue / 8), 0);
+        Assert.AreEqual(PlayerLevelCalculator.GetExperienceFromGemsSpent(Double.MaxValue / 8), 0);
+        Assert.AreEqual(PlayerLevelCalculator.GetExperienceFromGemsSpent(1234124), 1234124 * 8);
+        Assert.AreEqual(PlayerLevelCalculator.GetExperienceFromGemsSpent(2), 2 * 8);
+        Assert.AreEqual(PlayerLevelCalculator.GetExperienceFromGemsSpent(1232), 1232 * 8);
+        Assert.AreEqual(PlayerLevelCalculator.GetExperienceFromGemsSpent(33), 33 * 8);
+    }
+
+    [Test]
+    public void TestGetExperienceFromMoney()
+    {
+        Assert.AreEqual(PlayerLevelCalculator.GetExperienceFromMoneySpent(0), 0.0);
+        Assert.AreEqual(PlayerLevelCalculator.GetExperienceFromMoneySpent(-1), 0.0);
+        Assert.AreEqual(PlayerLevelCalculator.GetExperienceFromMoneySpent(Double.MinValue), 0.0);
+        Assert.AreEqual(PlayerLevelCalculator.GetExperienceFromMoneySpent(Double.MaxValue), 0);
+        Assert.AreEqual(PlayerLevelCalculator.GetExperienceFromMoneySpent(Double.MaxValue / 2), 0);
+        Assert.AreEqual(PlayerLevelCalculator.GetExperienceFromMoneySpent(Double.MaxValue / 2), 0);
+        Assert.AreEqual(PlayerLevelCalculator.GetExperienceFromMoneySpent(1234124), 1234124 * 2);
+        Assert.AreEqual(PlayerLevelCalculator.GetExperienceFromMoneySpent(2), 2 * 2);
+        Assert.AreEqual(PlayerLevelCalculator.GetExperienceFromMoneySpent(1232), 1232 * 2);
+        Assert.AreEqual(PlayerLevelCalculator.GetExperienceFromMoneySpent(33), 33 * 2);
+    }
+
+    [Test]
+    public void TestGetExpToLevel()
+    {
+        Assert.AreEqual(PlayerLevelCalculator.GetExpToLevel(0), 20);
+        Assert.AreEqual(PlayerLevelCalculator.GetExpToLevel(-1), 0.0);
+        Assert.AreEqual(PlayerLevelCalculator.GetExpToLevel(int.MinValue), 0);
+        Assert.AreEqual(PlayerLevelCalculator.GetExpToLevel(int.MaxValue), PlayerLevelCalculator.GetExpToLevel(100));
+        Assert.AreEqual(PlayerLevelCalculator.GetExperienceToNextLevel(Double.MinValue), 0);
+        Assert.AreEqual(PlayerLevelCalculator.GetExperienceToNextLevel(Double.MaxValue), 0); // since not feseable we return 0
+        Assert.AreEqual(PlayerLevelCalculator.GetExperienceToNextLevel(103936), 1);
+        Assert.AreEqual(PlayerLevelCalculator.GetExperienceToNextLevel(343150), 10);
+        Assert.AreEqual(PlayerLevelCalculator.GetExperienceToNextLevel(342160), 1000);
+        Assert.AreEqual(PlayerLevelCalculator.GetExperienceToNextLevel(333160), 10000); 
+    }
+}
