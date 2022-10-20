9daa1b77420dbd9de32426c420587da92bd0e53b
pacmax2 <mgierschdev@gmail.com>
2022-10-20 14:45:58 -0400

    Adding object slider (Loading bar) on top of all objects

--- Assets/Scripts/Game/Controllers/Menu Controllers/MenuHandlerController.cs
+++ Assets/Scripts/Game/Controllers/Menu Controllers/MenuHandlerController.cs
@@ -344,7 +344,7 @@ public class MenuHandlerController : MonoBehaviour
 
             }
 
-            BussGrid.SetNotOwned(gameGridObject);
+            //BussGrid.SetNotOwned(gameGridObject);
         }
         else
         {
