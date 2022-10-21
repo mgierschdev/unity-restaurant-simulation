b22c1eb374435e87f84f3857fa55b0cb59c9aeda
pacmax2 <mgierschdev@gmail.com>
2022-10-21 17:21:28 -0400

    Improving loading screen

--- Assets/Scripts/Game/Controllers/SceneLoadController.cs
+++ Assets/Scripts/Game/Controllers/SceneLoadController.cs
@@ -10,7 +10,7 @@ using UnityEngine.UI;
 // Check the folder under Assets/Scenes
 public class SceneLoadController : MonoBehaviour
 {
-    private TextMeshProUGUI sliderProgress;
+    //private TextMeshProUGUI sliderProgress;
     private Slider slider;
     private FirebaseLoad firebase;
     private FirebaseAuth auth;
@@ -35,9 +35,9 @@ public class SceneLoadController : MonoBehaviour
         Util.IsNull(sliderGameObject, "SceneLoadController/Start Slider is null");
 
         // We get the text inside the slider
-        GameObject sliderProgressObject = GameObject.Find(Settings.SliderProgress).gameObject;
-        Util.IsNull(sliderProgressObject, "SceneLoadController/Start sliderProgressObject is null");
-        sliderProgress = sliderProgressObject.GetComponent<TextMeshProUGUI>();
+        // GameObject sliderProgressObject = GameObject.Find(Settings.SliderProgress).gameObject;
+        // Util.IsNull(sliderProgressObject, "SceneLoadController/Start sliderProgressObject is null");
+        // sliderProgress = sliderProgressObject.GetComponent<TextMeshProUGUI>();
 
         // Init firebase
         firebase = new FirebaseLoad();
@@ -64,7 +64,7 @@ public class SceneLoadController : MonoBehaviour
         currentTimeAtScene += Time.fixedDeltaTime;
         currentProgress = Mathf.Lerp(currentTimeAtScene / MIN_TIME_LOADING, 0.10f, Time.fixedDeltaTime);
         slider.value = currentProgress;
-        sliderProgress.text = "LOADING " + Mathf.Ceil(currentProgress * 100).ToString() + "%";
+        // sliderProgress.text = "LOADING " + Mathf.Ceil(currentProgress * 100).ToString() + "%";
 
         if (Mathf.Approximately(operation.progress, 0.9f) && userData != null && userData.IsCompleted && currentTimeAtScene > MIN_TIME_LOADING)
         {
