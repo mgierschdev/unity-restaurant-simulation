51be9b70be26e18b9935405607f135c1c8be4bd9
pacmax2 <mgierschdev@gmail.com>
2022-10-22 08:33:58 -0400

    Commenting old playmode tests

--- Assets/Scripts/Tests/PlayMode/TestNPCMovementPathFinding.cs
+++ Assets/Scripts/Tests/PlayMode/TestNPCMovementPathFinding.cs
@@ -5,72 +5,72 @@ using UnityEngine.TestTools;
 
 public class TestNPCMovementPathFinding
 {
-    private GameObject firstNPCObject;
-    private GameObject secondNPCObject;
-    private NPCController firstNPCController;
-    private NPCController secondNPCController;
-    private GameObject gridObject;
-    private Vector3Int initialTestingPosition;
+    // private GameObject firstNPCObject;
+    // private GameObject secondNPCObject;
+    // private NPCController firstNPCController;
+    // private NPCController secondNPCController;
+    // private GameObject gridObject;
+    // private Vector3Int initialTestingPosition;
 
-    [SetUp]
-    public void Setup()
-    {
-        // First NPC
-        firstNPCObject = Transform.Instantiate(Resources.Load(Settings.PrefabNpcClient, typeof(GameObject))) as GameObject;
-        firstNPCObject = Transform.Instantiate(Resources.Load(Settings.PrefabNpcClient, typeof(GameObject)), new Vector3(1, 1), Quaternion.identity) as GameObject;
-        firstNPCObject.transform.SetParent(gridObject.transform);
-        firstNPCController = firstNPCObject.GetComponent<NPCController>();
-        // Second NPC
-        secondNPCObject = Transform.Instantiate(Resources.Load(Settings.PrefabNpcClient, typeof(GameObject))) as GameObject;
-        secondNPCObject = Transform.Instantiate(Resources.Load(Settings.PrefabNpcClient, typeof(GameObject)), new Vector3(1, 1), Quaternion.identity) as GameObject;
-        secondNPCObject.transform.SetParent(gridObject.transform);
-        secondNPCController = secondNPCObject.GetComponent<NPCController>();
+    // [SetUp]
+    // public void Setup()
+    // {
+    //     // First NPC
+    //     firstNPCObject = Transform.Instantiate(Resources.Load(Settings.PrefabNpcClient, typeof(GameObject))) as GameObject;
+    //     firstNPCObject = Transform.Instantiate(Resources.Load(Settings.PrefabNpcClient, typeof(GameObject)), new Vector3(1, 1), Quaternion.identity) as GameObject;
+    //     firstNPCObject.transform.SetParent(gridObject.transform);
+    //     firstNPCController = firstNPCObject.GetComponent<NPCController>();
+    //     // Second NPC
+    //     secondNPCObject = Transform.Instantiate(Resources.Load(Settings.PrefabNpcClient, typeof(GameObject))) as GameObject;
+    //     secondNPCObject = Transform.Instantiate(Resources.Load(Settings.PrefabNpcClient, typeof(GameObject)), new Vector3(1, 1), Quaternion.identity) as GameObject;
+    //     secondNPCObject.transform.SetParent(gridObject.transform);
+    //     secondNPCController = secondNPCObject.GetComponent<NPCController>();
 
-        initialTestingPosition = new Vector3Int(1, 1);
-    }
+    //     initialTestingPosition = new Vector3Int(1, 1);
+    // }
 
 
-    [UnityTest]
-    public IEnumerator TestSimpleMoving()
-    {
-        int[] endPosition = new int[] { 4, 4 };
-        firstNPCController.Position = initialTestingPosition;
-        firstNPCController.SetSpeed(100);
-        firstNPCController.GoTo(new Vector3Int(endPosition[0], endPosition[1]));
-        yield return new WaitForSeconds(2f);
-        GameLog.Log(firstNPCController.Position);
-        Assert.AreEqual(firstNPCController.GetPositionAsArray()[0], endPosition[0]);
-        Assert.AreEqual(firstNPCController.GetPositionAsArray()[1], endPosition[1]);
-    }
+    // [UnityTest]
+    // public IEnumerator TestSimpleMoving()
+    // {
+    //     int[] endPosition = new int[] { 4, 4 };
+    //     firstNPCController.Position = initialTestingPosition;
+    //     firstNPCController.SetSpeed(100);
+    //     firstNPCController.GoTo(new Vector3Int(endPosition[0], endPosition[1]));
+    //     yield return new WaitForSeconds(2f);
+    //     GameLog.Log(firstNPCController.Position);
+    //     Assert.AreEqual(firstNPCController.GetPositionAsArray()[0], endPosition[0]);
+    //     Assert.AreEqual(firstNPCController.GetPositionAsArray()[1], endPosition[1]);
+    // }
 
-    [UnityTest]
-    public IEnumerator TestPathWithObstacles()
-    {
-        int[] endPosition = new int[] { 25, 14 };
-        BussGrid.SetTestGridObstacles(21, 1, 15);
-        firstNPCController.SetSpeed(100);
-        firstNPCController.GoTo(new Vector3Int(endPosition[0], endPosition[1]));
-        yield return new WaitForSeconds(2f);
-        Assert.AreEqual(firstNPCController.GetPositionAsArray()[0], endPosition[0]);
-        Assert.AreEqual(firstNPCController.GetPositionAsArray()[1], endPosition[1]);
-        BussGrid.FreeTestGridObstacles(5, 1, 15);
-    }
+    // [UnityTest]
+    // public IEnumerator TestPathWithObstacles()
+    // {
+    //     int[] endPosition = new int[] { 25, 14 };
+    //     BussGrid.SetTestGridObstacles(21, 1, 15);
+    //     firstNPCController.SetSpeed(100);
+    //     firstNPCController.GoTo(new Vector3Int(endPosition[0], endPosition[1]));
+    //     yield return new WaitForSeconds(2f);
+    //     Assert.AreEqual(firstNPCController.GetPositionAsArray()[0], endPosition[0]);
+    //     Assert.AreEqual(firstNPCController.GetPositionAsArray()[1], endPosition[1]);
+    //     BussGrid.FreeTestGridObstacles(5, 1, 15);
+    // }
 
-    [UnityTest]
-    //They can overlap
-    public IEnumerator TestMultipleNPC()
-    {
-        int[] endPosition = new int[] { 25, 14 };
-        BussGrid.SetTestGridObstacles(21, 1, 15);
-        firstNPCController.SetSpeed(100);
-        secondNPCController.SetSpeed(100);
-        firstNPCController.GoTo(new Vector3Int(endPosition[0], endPosition[1]));
-        secondNPCController.GoTo(new Vector3Int(endPosition[0], endPosition[1]));
-        yield return new WaitForSeconds(2f);
-        Assert.AreEqual(secondNPCController.GetPositionAsArray()[0], endPosition[0]);
-        Assert.AreEqual(secondNPCController.GetPositionAsArray()[1], endPosition[1]);
-        Assert.AreEqual(firstNPCController.GetPositionAsArray()[0], endPosition[0]);
-        Assert.AreEqual(firstNPCController.GetPositionAsArray()[1], endPosition[1]);
-        BussGrid.FreeTestGridObstacles(5, 1, 15);
-    }
+    // [UnityTest]
+    // //They can overlap
+    // public IEnumerator TestMultipleNPC()
+    // {
+    //     int[] endPosition = new int[] { 25, 14 };
+    //     BussGrid.SetTestGridObstacles(21, 1, 15);
+    //     firstNPCController.SetSpeed(100);
+    //     secondNPCController.SetSpeed(100);
+    //     firstNPCController.GoTo(new Vector3Int(endPosition[0], endPosition[1]));
+    //     secondNPCController.GoTo(new Vector3Int(endPosition[0], endPosition[1]));
+    //     yield return new WaitForSeconds(2f);
+    //     Assert.AreEqual(secondNPCController.GetPositionAsArray()[0], endPosition[0]);
+    //     Assert.AreEqual(secondNPCController.GetPositionAsArray()[1], endPosition[1]);
+    //     Assert.AreEqual(firstNPCController.GetPositionAsArray()[0], endPosition[0]);
+    //     Assert.AreEqual(firstNPCController.GetPositionAsArray()[1], endPosition[1]);
+    //     BussGrid.FreeTestGridObstacles(5, 1, 15);
+    // }
 }
--- Assets/Scripts/Tests/PlayMode/TestPlayerMovementPathFinding.cs
+++ Assets/Scripts/Tests/PlayMode/TestPlayerMovementPathFinding.cs
@@ -5,45 +5,45 @@ using UnityEngine.TestTools;
 
 public class TestPlayerMovementPathFinding
 {
-    private GameObject playerObject;
-    private PlayerController playerController;
-    private GameObject gridObject;
-    private Vector3Int initialTestingPosition;
+    // private GameObject playerObject;
+    // private PlayerController playerController;
+    // private GameObject gridObject;
+    // private Vector3Int initialTestingPosition;
 
-    [SetUp]
-    public void Setup()
-    {
-        initialTestingPosition = new Vector3Int(1, 1);
-        // Player
-        playerObject = Transform.Instantiate(Resources.Load(Settings.PrefabPlayer, typeof(GameObject))) as GameObject;
-        playerObject = Transform.Instantiate(Resources.Load(Settings.PrefabPlayer, typeof(GameObject)), initialTestingPosition, Quaternion.identity) as GameObject;
-        playerObject.transform.SetParent(gridObject.transform);
-        playerController = playerObject.GetComponent<PlayerController>();
-    }
+    // [SetUp]
+    // public void Setup()
+    // {
+    //     initialTestingPosition = new Vector3Int(1, 1);
+    //     // Player
+    //     playerObject = Transform.Instantiate(Resources.Load(Settings.PrefabPlayer, typeof(GameObject))) as GameObject;
+    //     playerObject = Transform.Instantiate(Resources.Load(Settings.PrefabPlayer, typeof(GameObject)), initialTestingPosition, Quaternion.identity) as GameObject;
+    //     playerObject.transform.SetParent(gridObject.transform);
+    //     playerController = playerObject.GetComponent<PlayerController>();
+    // }
 
-    [UnityTest]
-    public IEnumerator TestSimplePath()
-    {
-        int[] endPosition = new int[] { 25, 14 };
-        playerController.SetSpeed(100);
-        playerController.GoTo(new Vector3Int(endPosition[0], endPosition[1]));
-        yield return new WaitForSeconds(1f);
-        Assert.AreEqual(playerController.GetPositionAsArray()[0], endPosition[0]);
-        Assert.AreEqual(playerController.GetPositionAsArray()[1], endPosition[1]);
-    }
+    // [UnityTest]
+    // public IEnumerator TestSimplePath()
+    // {
+    //     int[] endPosition = new int[] { 25, 14 };
+    //     playerController.SetSpeed(100);
+    //     playerController.GoTo(new Vector3Int(endPosition[0], endPosition[1]));
+    //     yield return new WaitForSeconds(1f);
+    //     Assert.AreEqual(playerController.GetPositionAsArray()[0], endPosition[0]);
+    //     Assert.AreEqual(playerController.GetPositionAsArray()[1], endPosition[1]);
+    // }
 
-    [UnityTest]
-    public IEnumerator TestPathWithObstacles()
-    {
-        int[] endPosition = new int[] { 25, 14 };
-        BussGrid.SetTestGridObstacles(21, 1, 15);
-        //playerController.Position = gameGridController.GetWorldFromPathFindingGridPosition(new Vector3Int(startPosition[0], startPosition[1]));
-        playerController.SetSpeed(100);
-        playerController.GoTo(new Vector3Int(endPosition[0], endPosition[1]));
-        yield return new WaitForSeconds(1f);
-        GameLog.Log(playerController.Position);
-        Assert.AreEqual(playerController.GetPositionAsArray()[0], endPosition[0]);
-        Assert.AreEqual(playerController.GetPositionAsArray()[1], endPosition[1]);
-        BussGrid.FreeTestGridObstacles(5, 1, 15);
-    }
+    // [UnityTest]
+    // public IEnumerator TestPathWithObstacles()
+    // {
+    //     int[] endPosition = new int[] { 25, 14 };
+    //     BussGrid.SetTestGridObstacles(21, 1, 15);
+    //     //playerController.Position = gameGridController.GetWorldFromPathFindingGridPosition(new Vector3Int(startPosition[0], startPosition[1]));
+    //     playerController.SetSpeed(100);
+    //     playerController.GoTo(new Vector3Int(endPosition[0], endPosition[1]));
+    //     yield return new WaitForSeconds(1f);
+    //     GameLog.Log(playerController.Position);
+    //     Assert.AreEqual(playerController.GetPositionAsArray()[0], endPosition[0]);
+    //     Assert.AreEqual(playerController.GetPositionAsArray()[1], endPosition[1]);
+    //     BussGrid.FreeTestGridObstacles(5, 1, 15);
+    // }
 }
