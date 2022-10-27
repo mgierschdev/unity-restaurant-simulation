ed54c0ceb13023c05f4b41a390c30857450825d0
pacmax2 <mgierschdev@gmail.com>
2022-10-27 10:39:01 -0400

    Refactoring main controllers

--- Assets/Scripts/Game/Controllers/Grid Objects Controllers/TableController.cs
+++ Assets/Scripts/Game/Controllers/Grid Objects Controllers/TableController.cs
@@ -1,9 +1,4 @@
 public class TableController : BaseObjectController
 {
-    private void Start()
-    {
-        Init();
-        gameGridObject = new GameGridObject(transform, MenuObjectList.GetStoreObject(StoreItemType.WOODEN_TABLE_SINGLE));
-        BussGrid.SetGridObject(gameGridObject);
-    }
+    
 }
