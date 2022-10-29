428e7a8c8f6fe197b68b9d9cff7c07fb52461c4b
pacmax2 <mgierschdev@gmail.com>
2022-10-29 09:09:30 -0400

    Increase accuracy of dragging // GetMouseOnGameGridPosition

--- Assets/Scripts/Game/Grid/GameTile.cs
+++ Assets/Scripts/Game/Grid/GameTile.cs
@@ -6,12 +6,17 @@ public class GameTile : GameObjectBase
     public GameTile(Vector3 worldPosition, Vector3Int gridPosition, Vector3Int localGridPosition, TileType name, ObjectType type, TileBase unityTileBase)
     {
         // Grid position, first position = 0, 0
-        GridPosition = gridPosition; 
+        GridPosition = gridPosition;
         // World position on Unity coords
         WorldPosition = worldPosition;
         LocalGridPosition = localGridPosition;
         TileType = name;
         Type = type;
         UnityTileBase = unityTileBase;
+    }
+    
+    public override string ToString()
+    {
+        return WorldPosition + " " + GridPosition + " " + LocalGridPosition + " " + Name + " " + Type + " ";
     }
 }
