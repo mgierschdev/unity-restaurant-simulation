3d95c039dffffca41ed6169208803e8d8773ff24
pacmax2 <mgierschdev@gmail.com>
2022-11-02 14:13:52 -0400

    Adding the topItemController

---
+++ Assets/Scripts/Game/Controllers/Grid Objects Controllers/TopItemController.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+// Control the top item movement between gamegrid containers
+// this script is attached to the base game object
+public class TopItemController : MonoBehaviour
+{
+
+}
