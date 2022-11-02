c890a308fda79967936670df7124091c2bde3d9c
pacmax2 <mgierschdev@gmail.com>
2022-11-02 14:32:09 -0400

    Adding clickListeners for the topItem editPanel

--- Assets/Scripts/Game/Controllers/Grid Objects Controllers/TopItemController.cs
+++ Assets/Scripts/Game/Controllers/Grid Objects Controllers/TopItemController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.U2D.Animation;
+using UnityEngine.UI;
 
 // Control the top item movement between gamegrid containers
 // this script is attached to the base -> BaseObject/Object/TopObject
@@ -8,12 +9,30 @@ public class TopItemController : MonoBehaviour
     private SpriteRenderer spriteRenderer;
     private SpriteResolver spriteResolver;
     private StoreGameObject obj;
+    private GameObject saveObjButton, acceptButton, cancelButton;
 
     public void Start()
     {
         obj = null;
         spriteRenderer = transform.GetComponent<SpriteRenderer>();
         spriteResolver = transform.GetComponent<SpriteResolver>();
+        SetEditPanelButtonClickListeners();
+        HideEditPanel();
+    }
+
+    private void SetEditPanelButtonClickListeners()
+    {
+        saveObjButton = transform.Find(Settings.ConstEditStoreMenuSave).gameObject;
+        Button save = saveObjButton.GetComponent<Button>();
+        save.onClick.AddListener(ButtonsClickListener);
+
+        acceptButton = transform.transform.Find(Settings.ConstEditStoreMenuButtonAccept).gameObject;
+        Button accept = acceptButton.GetComponent<Button>();
+        accept.onClick.AddListener(ButtonsClickListener);
+
+        cancelButton = transform.transform.Find(Settings.ConstEditStoreMenuButtonCancel).gameObject;
+        Button cancel = cancelButton.GetComponent<Button>();
+        cancel.onClick.AddListener(ButtonsClickListener);
     }
 
     public void HideTopItem()
@@ -39,6 +58,7 @@ public class TopItemController : MonoBehaviour
         {
             spriteResolver.SetCategoryAndLabel(obj.SpriteLibCategory, obj.Identifier);
             ShowTopItem();
+            ShowEditPanel();
             this.obj = obj;
         }
         else
@@ -50,5 +70,24 @@ public class TopItemController : MonoBehaviour
     public StoreGameObject GetTopItem()
     {
         return obj;
+    }
+
+    public void ButtonsClickListener()
+    {
+        Debug.Log("Click listener ");
+    }
+
+    public void ShowEditPanel()
+    {
+        saveObjButton.SetActive(false);
+        acceptButton.SetActive(false);
+        cancelButton.SetActive(false);
+    }
+
+    public void HideEditPanel()
+    {
+        saveObjButton.SetActive(true);
+        acceptButton.SetActive(true);
+        cancelButton.SetActive(true);
     }
 }
