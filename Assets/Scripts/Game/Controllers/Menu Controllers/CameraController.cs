58cfae88db5135181161e25a0f2b49d28201f155
pacmax2 <mgierschdev@gmail.com>
2022-11-04 12:50:45 -0400

    item are not loading by default

--- Assets/Scripts/Game/Controllers/Menu Controllers/CameraController.cs
+++ Assets/Scripts/Game/Controllers/Menu Controllers/CameraController.cs
@@ -147,7 +147,7 @@ public class CameraController : MonoBehaviour
 
     private IEnumerator DisablePerspectiveHandEnum()
     {
-        yield return new WaitForSeconds(0.1f);
+        yield return new WaitForSeconds(0.3f);
         IsPerspectiveHandTempDisabled = false;
     }
 
