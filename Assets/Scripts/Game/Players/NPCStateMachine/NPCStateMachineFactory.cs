b7b5cea0e81cebab29328777a98d9b8185fa6359
pacmax2 <mgierschdev@gmail.com>
2022-11-07 09:11:19 -0400

    Moving cameraScale to the Util folder, npc controller refactor

--- Assets/Scripts/Game/Players/NPCStateMachine/NPCStateMachineFactory.cs
+++ Assets/Scripts/Game/Players/NPCStateMachine/NPCStateMachineFactory.cs
@@ -11,12 +11,17 @@ public static class NPCStateMachineFactory
         bool[] nodeTransition = new bool[Enum.GetNames(typeof(NpcStateTransitions)).Length];
 
         //IDLE -> Other
+        nodeTransition[(int)NpcStateTransitions.NPC_IS_NOT_MOVING] = true;
         nodeTransition[(int)NpcStateTransitions.TABLE_AVAILABLE] = true;
         adjMatrix[(int)NpcState.IDLE, (int)NpcState.WALKING_TO_TABLE] = new StateNodeTransition((bool[])nodeTransition.Clone());
         Array.Fill(nodeTransition, false);
+
+        nodeTransition[(int)NpcStateTransitions.NPC_IS_NOT_MOVING] = true;
         nodeTransition[(int)NpcStateTransitions.WANDER] = true;
         adjMatrix[(int)NpcState.IDLE, (int)NpcState.WANDER] = new StateNodeTransition((bool[])nodeTransition.Clone());
         Array.Fill(nodeTransition, false);
+
+        nodeTransition[(int)NpcStateTransitions.NPC_IS_NOT_MOVING] = true;
         nodeTransition[(int)NpcStateTransitions.TABLE_MOVED] = true;
         nodeTransition[(int)NpcStateTransitions.WALK_TO_UNRESPAWN] = true;
         adjMatrix[(int)NpcState.IDLE, (int)NpcState.WALKING_UNRESPAWN] = new StateNodeTransition((bool[])nodeTransition.Clone());
@@ -26,14 +31,19 @@ public static class NPCStateMachineFactory
         nodeTransition[(int)NpcStateTransitions.NPC_IS_NOT_MOVING] = true;
         adjMatrix[(int)NpcState.WANDER, (int)NpcState.IDLE] = new StateNodeTransition((bool[])nodeTransition.Clone());
         Array.Fill(nodeTransition, false);
+        
+        nodeTransition[(int)NpcStateTransitions.NPC_IS_NOT_MOVING] = true;
         nodeTransition[(int)NpcStateTransitions.WALK_TO_UNRESPAWN] = true;
         adjMatrix[(int)NpcState.WANDER, (int)NpcState.WALKING_UNRESPAWN] = new StateNodeTransition((bool[])nodeTransition.Clone());
         Array.Fill(nodeTransition, false);
 
         //WALKING_TO_TABLE -> Other
+        nodeTransition[(int)NpcStateTransitions.NPC_IS_NOT_MOVING] = true;
         nodeTransition[(int)NpcStateTransitions.TABLE_AVAILABLE] = true;
         adjMatrix[(int)NpcState.WALKING_TO_TABLE, (int)NpcState.AT_TABLE] = new StateNodeTransition((bool[])nodeTransition.Clone());
         Array.Fill(nodeTransition, false);
+
+        nodeTransition[(int)NpcStateTransitions.NPC_IS_NOT_MOVING] = true;
         nodeTransition[(int)NpcStateTransitions.TABLE_MOVED] = true;
         nodeTransition[(int)NpcStateTransitions.WALK_TO_UNRESPAWN] = true;
         adjMatrix[(int)NpcState.WALKING_TO_TABLE, (int)NpcState.WALKING_UNRESPAWN] = new StateNodeTransition((bool[])nodeTransition.Clone());
@@ -43,30 +53,38 @@ public static class NPCStateMachineFactory
         nodeTransition[(int)NpcStateTransitions.NPC_IS_NOT_MOVING] = true;
         adjMatrix[(int)NpcState.AT_TABLE, (int)NpcState.WAITING_TO_BE_ATTENDED] = new StateNodeTransition((bool[])nodeTransition.Clone());
         Array.Fill(nodeTransition, false);
+
+        nodeTransition[(int)NpcStateTransitions.NPC_IS_NOT_MOVING] = true;
         nodeTransition[(int)NpcStateTransitions.TABLE_MOVED] = true;
         nodeTransition[(int)NpcStateTransitions.WALK_TO_UNRESPAWN] = true;
         adjMatrix[(int)NpcState.AT_TABLE, (int)NpcState.WALKING_UNRESPAWN] = new StateNodeTransition((bool[])nodeTransition.Clone());
         Array.Fill(nodeTransition, false);
 
         //WAITING_TO_BE_ATTENDED -> Other
+        nodeTransition[(int)NpcStateTransitions.NPC_IS_NOT_MOVING] = true;
         nodeTransition[(int)NpcStateTransitions.BEING_ATTENDED] = true;
         adjMatrix[(int)NpcState.WAITING_TO_BE_ATTENDED, (int)NpcState.BEING_ATTENDED] = new StateNodeTransition((bool[])nodeTransition.Clone());
         Array.Fill(nodeTransition, false);
+
+        nodeTransition[(int)NpcStateTransitions.NPC_IS_NOT_MOVING] = true;
         nodeTransition[(int)NpcStateTransitions.TABLE_MOVED] = true;
         nodeTransition[(int)NpcStateTransitions.WALK_TO_UNRESPAWN] = true;
         adjMatrix[(int)NpcState.WAITING_TO_BE_ATTENDED, (int)NpcState.WALKING_UNRESPAWN] = new StateNodeTransition((bool[])nodeTransition.Clone());
         Array.Fill(nodeTransition, false);
 
         //BEING_ATTENDED -> Other
+        nodeTransition[(int)NpcStateTransitions.NPC_IS_NOT_MOVING] = true;
         nodeTransition[(int)NpcStateTransitions.ATTENDED] = true;
         adjMatrix[(int)NpcState.BEING_ATTENDED, (int)NpcState.ATTENDED] = new StateNodeTransition((bool[])nodeTransition.Clone());
         Array.Fill(nodeTransition, false);
+
         nodeTransition[(int)NpcStateTransitions.TABLE_MOVED] = true;
         nodeTransition[(int)NpcStateTransitions.WALK_TO_UNRESPAWN] = true;
         adjMatrix[(int)NpcState.BEING_ATTENDED, (int)NpcState.WALKING_UNRESPAWN] = new StateNodeTransition((bool[])nodeTransition.Clone());
         Array.Fill(nodeTransition, false);
 
         //ATTENDED -> Other
+        nodeTransition[(int)NpcStateTransitions.NPC_IS_NOT_MOVING] = true;
         nodeTransition[(int)NpcStateTransitions.ORDER_SERVED] = true;
         nodeTransition[(int)NpcStateTransitions.TABLE_MOVED] = true;
         nodeTransition[(int)NpcStateTransitions.WALK_TO_UNRESPAWN] = true;
---
+++ Assets/Scripts/Util/Unity/CameraScale.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class CameraScale : MonoBehaviour
+{
+    private float currentWindowAspectRatio;
+    private float targetSpectRatio;
+    private Camera mainCamera;
+
+    private void Start()
+    {
+        mainCamera = GetComponent<Camera>();
+
+        ScaleCamera();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+#if UNITY_EDITOR
+        if (mainCamera)
+        {
+            ScaleCamera();
+        }
+#endif
+    }
+
+    // Failing with Samsumg foldable devices and some devices by half unit at the end of the screen
+    // (LG Nexus4, Ipad Pro 12.9/11 2units at the end), Ipad Mini, Ipad Air, 
+    private void ScaleCamera()
+    {
+        currentWindowAspectRatio = (float)Screen.width / (float)Screen.height;
+        targetSpectRatio = (float)Settings.ConstDefaultCameraWidth / (float)Settings.ConstDefaultCameraHeight;
+        // should be scaled to this ammount
+        float newScaleHeight = currentWindowAspectRatio / targetSpectRatio;
+        if (newScaleHeight > 1)
+        {
+            mainCamera.orthographicSize = Settings.ConstDefaultCameraOrthographicsize - (newScaleHeight - 1) * Settings.ConstDefaultCameraOrthographicsize;
+        }
+    }
+}
