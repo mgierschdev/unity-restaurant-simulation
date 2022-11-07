37d6a9e5a728e8ef73d1f10b51be3f62d755d22c
pacmax2 <mgierschdev@gmail.com>
2022-11-07 19:46:45 -0400

    refactoring the state machine for a generic state machine

--- Assets/Scripts/Tests/EditMode/TestNPCStateMachine.cs
+++ Assets/Scripts/Tests/EditMode/TestNPCStateMachine.cs
@@ -8,16 +8,25 @@ public class TestNPCStateMachine
     [Test]
     public void TestNPCStateMachineClient()
     {
-        StateMachine stateMachine = NPCStateMachineFactory.GetClientStateMachine();
+        StateMachine<NpcState, NpcStateTransitions> stateMachine = NPCStateMachineFactory.GetClientStateMachine();
+        stateMachine.printStateMachine();
+        Assert.True(AssertStates(stateMachine.Map[NpcState.IDLE], 9));
+    }
+
+    [Test]
+    public void TestNPCStateMachineEmployee()
+    {
+        StateMachine<NpcState, NpcStateTransitions> stateMachine = NPCStateMachineFactory.GetEmployeeStateMachine();
+        stateMachine.printStateMachine();
         Assert.True(AssertStates(stateMachine.Map[NpcState.IDLE], 9));
     }
 
     // connected states not necesarily all of them
-    private bool AssertStates(StateMachineNode start, int states)
+    private bool AssertStates(StateMachineNode<NpcState> start, int states)
     {
         int count = 0;
-        Queue<StateMachineNode> queue = new Queue<StateMachineNode>();
-        HashSet<StateMachineNode> visited = new HashSet<StateMachineNode>();
+        Queue<StateMachineNode<NpcState>> queue = new Queue<StateMachineNode<NpcState>>();
+        HashSet<StateMachineNode<NpcState>> visited = new HashSet<StateMachineNode<NpcState>>();
         queue.Enqueue(start);
 
         while (queue.Count != 0)
@@ -26,10 +35,10 @@ public class TestNPCStateMachine
 
             while (size-- > 0)
             {
-                StateMachineNode current = queue.Dequeue();
+                StateMachineNode<NpcState> current = queue.Dequeue();
                 count++;
                 visited.Add(current);
-                foreach (StateMachineNode node in current.TransitionStates)
+                foreach (StateMachineNode<NpcState> node in current.TransitionStates)
                 {
                     if (!visited.Contains(node))
                     {
