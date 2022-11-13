9d8924c07115d966f8924380c7dd4de7f2b61a30
pacmax2 <mgierschdev@gmail.com>
2022-11-13 10:32:32 -0400

    refactoring config

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -1,175 +1,174 @@
 public static class Settings
 {
     //DEBUG/DEV parameterss
-    public const bool IsFirebaseEmulatorEnabled = true;
-    public const bool CellDebug = true;
+    public const bool IsFirebaseEmulatorEnabled = true,
+    CellDebug = true;
     public const int DebugTextSize = 9;
     public const float NpcDefaultMovementSpeed = 3f;
 
     //Player Config
     public const bool PlayerWalkOnClick = true;
     public const float PlayerMovementSpeed = 3f;
-    public const int  MaxNpcNumber = 11; // Performance relevant, 60fps 11-12
-    public static int[] 
-    StartContainer = new int[] { 4, 13 },
+    public const int MaxNpcNumber = 11; // Performance relevant, 60fps 11-12
+    public static int[] StartContainer = new int[] { 4, 13 },
     StartTable = new int[] { 4, 11 };
 
     //Init start attributes
     public static int InitGameMoney = 2000,
-                InitGems = 40,
-                InitExperience = 0,
-                InitLevel = 0,
-                InitGridSize = 4;
+    InitGems = 40,
+    InitExperience = 0,
+    InitLevel = 0,
+    InitGridSize = 4;
 
     //Sliders 
-    public const float ObjectMoveSliderMultiplayer = 0.8f;
-    public const float ItemLoadSliderMultiplayer = 0.05f;
-    public const float ScreenLoadTime = 2f;
-    public const float TimeBeforeTheSliderIsEnabled = 0.4f;
+    public const float ObjectMoveSliderMultiplayer = 0.8f,
+    ItemLoadSliderMultiplayer = 0.05f,
+    ScreenLoadTime = 2f,
+    TimeBeforeTheSliderIsEnabled = 0.4f;
 
     //Waiting times
-    public const float NPCMaxWaitingTime = 10f;
-    public const float NPCMaxTimeInState = 10f;
-    public const double MIN_EUCLIDIAN_DISTANCE_RANDOM_WALK = 10; // Performance relevant
+    public const float NPCMaxWaitingTime = 10f,
+    NPCMaxTimeInState = 10f,
+    MinEuclidianDistanceRandomWalk = 10; // Performance relevant
 
     //FIREBASE TEST ENV 
-    public const string FIRESTORE_HOST = "localhost:8080";
-    public const string CLOUD_FUNCTION_HOST = IsFirebaseEmulatorEnabled ? "localhost:5001" : "";
-    public const string USER_PRED_PROD_COLLECTION = "PreProd";
-    public const string TEST_USER = "TESTUSERID";
+    public const string FIRESTORE_HOST = "localhost:8080",
+    LOUD_FUNCTION_HOST = IsFirebaseEmulatorEnabled ? "localhost:5001" : "",
+    USER_PRED_PROD_COLLECTION = "PreProd",
+    TEST_USER = "TESTUSERID";
 
     //GAME TAGS
-    public const string gameName = "Idle Tycoon - Restaurant";
-    public const string NpcTag = "NPC";
-    public const string NpcEmployeeTag = "Employee";
-    public const string SliderTag = "Slider";
+    public const string gameName = "Idle Tycoon - Restaurant",
+    NpcTag = "NPC",
+    NpcEmployeeTag = "Employee",
+    SliderTag = "Slider";
 
     //SCENES
-    public const string LoadScene = "LoadScene";
-    public const string GameScene = "GameScene";
-    public const string SliderProgress = "SliderProgress";
+    public const string LoadScene = "LoadScene",
+    GameScene = "GameScene",
+    SliderProgress = "SliderProgress";
 
     //STORE SPRITES
     public const string StoreSpritePath = "Objects/Sprites/";
 
     // TILEMAPS
-    public const string TilemapFloor0 = "TilemapFloor0";
-    public const string TilemapColliders = "TilemapColliders";
-    public const string TilemapObjects = "TilemapObjects";
-    public const string TilemapWalkingPath = "TilemapWalkingPath";
-    public const string TilemapBusinessFloor = "TilemapBusinessFloor";
-    public const string TilemapBusinessFloor_2 = "TilemapBusinessFloor_2";
-    public const string TilemapBusinessFloor_3 = "TilemapBusinessFloor_3";
-    public const string TilemapBusinessFloor_4 = "TilemapBusinessFloor_4";
-    public const string TilemapBusinessFloor_5 = "TilemapBusinessFloor_5";
-    public const string TilemapBusinessFloor_6 = "TilemapBusinessFloor_6";
-    public const string TilemapBusinessFloor_7 = "TilemapBusinessFloor_7";
-    public const string TilemapBusinessFloor_8 = "TilemapBusinessFloor_8";
-    public const string TilemapBusinessFloor_9 = "TilemapBusinessFloor_9";
-    public const string TilemapBusinessFloor_10 = "TilemapBusinessFloor_10";
-    public const string PathFindingGrid = "PathFindingGrid";
+    public const string TilemapFloor0 = "TilemapFloor0",
+    TilemapColliders = "TilemapColliders",
+    TilemapObjects = "TilemapObjects",
+    TilemapWalkingPath = "TilemapWalkingPath",
+    TilemapBusinessFloor = "TilemapBusinessFloor",
+    TilemapBusinessFloor_2 = "TilemapBusinessFloor_2",
+    TilemapBusinessFloor_3 = "TilemapBusinessFloor_3",
+    TilemapBusinessFloor_4 = "TilemapBusinessFloor_4",
+    TilemapBusinessFloor_5 = "TilemapBusinessFloor_5",
+    TilemapBusinessFloor_6 = "TilemapBusinessFloor_6",
+    TilemapBusinessFloor_7 = "TilemapBusinessFloor_7",
+    TilemapBusinessFloor_8 = "TilemapBusinessFloor_8",
+    TilemapBusinessFloor_9 = "TilemapBusinessFloor_9",
+    TilemapBusinessFloor_10 = "TilemapBusinessFloor_10",
+    PathFindingGrid = "PathFindingGrid";
 
     //SPRITE LIBS CATEGORIES
-    public const string SpriteLibCategoryTables = "Tables";
-    public const string SpriteLibCategoryDispensers = "Dispensers";
-    public const string SpriteLibCategoryStoreObjects = "StoreObjects";
+    public const string SpriteLibCategoryTables = "Tables",
+    SpriteLibCategoryDispensers = "Dispensers",
+    SpriteLibCategoryStoreObjects = "StoreObjects";
 
     //PREFABS AND OBJECTS
-    public const string Button = "Button";
-    public const string PrefabGridTile = "Grid/GridTile";
-    public const string PrefabSingleTable = "Objects/SingleTable";
-    public const string PrefabCounter = "Objects/Counter";
-    public const string PrefabBaseDispenser = "Objects/BaseDispenser";
-    public const string ObjectRotationFrontInverted = "Inverted";
-    public const string ObjectRotationFront = "Front";
-    public const string PrefabPlayer = "Players/Player";
-    public const string PrefabNpcClient = "Players/Client";
-    public const string PrefabNpcEmployee = "Players/Employee";
-    public const string PrefabInventoryItem = "Menu/InventoryItem";
-    public const string MenuBackground = "MenuBackground";
-    public const string MenuContainer = "MenuContainer";
-    public const string SideMenuButton = "Menu/CenterTabMenuBotton";
-    public const string PrefabInventoryItemImage = "Image";
-    public const string PrefabMenuInventoryItemImage = "Image/ItemImage";
-    public const string PrefabInventoryItemTextPrice = "Image/TextBackground/Price";
-    public const string BaseObjectUnderTile = "Object/Tiles/UnderTile";
-    public const string BaseObjectActionTile = "Object/Tiles/ActionTile";
-    public const string BaseObjectActionTile2 = "Object/Tiles/ActionTile2";
-    public const string BaseObjectSpriteRenderer = "Object";
-    public const string BaseObjectTopObject = "TopObject";
-    public const string undefined = "undefined";
+    public const string Button = "Button",
+    PrefabGridTile = "Grid/GridTile",
+    PrefabSingleTable = "Objects/SingleTable",
+    PrefabCounter = "Objects/Counter",
+    PrefabBaseDispenser = "Objects/BaseDispenser",
+    ObjectRotationFrontInverted = "Inverted",
+    ObjectRotationFront = "Front",
+    PrefabPlayer = "Players/Player",
+    PrefabNpcClient = "Players/Client",
+    refabNpcEmployee = "Players/Employee",
+    PrefabInventoryItem = "Menu/InventoryItem",
+    MenuBackground = "MenuBackground",
+    MenuContainer = "MenuContainer",
+    SideMenuButton = "Menu/CenterTabMenuBotton",
+    PrefabInventoryItemImage = "Image",
+    PrefabMenuInventoryItemImage = "Image/ItemImage",
+    PrefabInventoryItemTextPrice = "Image/TextBackground/Price",
+    BaseObjectUnderTile = "Object/Tiles/UnderTile",
+    BaseObjectActionTile = "Object/Tiles/ActionTile",
+    BaseObjectActionTile2 = "Object/Tiles/ActionTile2",
+    BaseObjectSpriteRenderer = "Object",
+    BaseObjectTopObject = "TopObject",
+    undefined = "undefined";
 
     //NPC Default
     public const float MinDistanceToTarget = 0.001f;
     public const int NpcDefaultEnergy = 100;
-    public const string NpcEnergyBar = "EnergyBar";
-    public const string NpcCharacter = "Character";
+    public const string NpcEnergyBar = "EnergyBar",
+    NpcCharacter = "Character";
 
     //UI Constants
-    public const string ConstNpcProfileMenu = "NPCProfile";
-    public const string ConstCanvasParentMenu = "CanvasMenu";
-    public const string ConstTopMenuDisplayMoney = "MoneyLabel";
-    public const string ConstTopMenuDisplayGems = "GemMoneyLabel";
-    public const string ConstTopMenuLevel = "LevelLabel";
-    public const string ConstTopMenuExpSlider = "ExperienceSlider";
-    public const string ConstCenterTabMenu = "CenterPanel";
-    public const string ConstCenterScrollView = "ViewPanel/MainContent/ScrollView";
-    public const string ConstCenterScrollContent = "ViewPanel/MainContent/ScrollView/Viewport/Content";
-    public const string ConstLeftDownPanel = "LeftDownPanel";
-    public const string ConstEditItemMenuPanel = "EditPanelItem";
-    public const string ConstEditTopItemMenuPanel = "EditPanelTopItem";
-    public const string ConstEditStoreMenuCancel = "Cancel";
-    public const string ConstEditStoreMenuAccept = "Accept";
-    public const string ConstEditStoreMenuRotateLeft = "ButtonRotateLeft";
-    public const string ConstEditStoreMenuButtonAccept = "ButtonAccept";
-    public const string ConstEditStoreMenuButtonCancel = "ButtonCancel";
+    public const string ConstNpcProfileMenu = "NPCProfile",
+    ConstCanvasParentMenu = "CanvasMenu",
+    ConstTopMenuDisplayMoney = "MoneyLabel",
+    ConstTopMenuDisplayGems = "GemMoneyLabel",
+    ConstTopMenuLevel = "LevelLabel",
+    ConstTopMenuExpSlider = "ExperienceSlider",
+    ConstCenterTabMenu = "CenterPanel",
+    ConstCenterScrollView = "ViewPanel/MainContent/ScrollView",
+    ConstCenterScrollContent = "ViewPanel/MainContent/ScrollView/Viewport/Content",
+    ConstLeftDownPanel = "LeftDownPanel",
+    ConstEditItemMenuPanel = "EditPanelItem",
+    ConstEditTopItemMenuPanel = "EditPanelTopItem",
+    ConstEditStoreMenuCancel = "Cancel",
+    ConstEditStoreMenuAccept = "Accept",
+    ConstEditStoreMenuRotateLeft = "ButtonRotateLeft",
+    ConstEditStoreMenuButtonAccept = "ButtonAccept",
+    ConstEditStoreMenuButtonCancel = "ButtonCancel";
 
-    public const string ConstEditStoreMenuSave = "ButtonSave";
-    public const string ConstParentGameObject = "Game";
-    public const int ConstDefaultCameraOrthographicsize = 7;
-    public const int ConstDefaultBackgroundOrderingLevel = 200;
+    public const string ConstEditStoreMenuSave = "ButtonSave",
+    ConstParentGameObject = "Game";
+    public const int ConstDefaultCameraOrthographicsize = 7,
+    ConstDefaultBackgroundOrderingLevel = 200;
 
     //UI: Menu
     public const string ConstLeftDownMenuStore = "Store";
 
     //UI: Camera
     public const bool CameraPerspectiveHand = true; //CAMERA_PERSPECTIVE_HAND or CAMERA_FOLLOW_PLAYER
-    public static float[] CameraPerspectiveHandClampX = { -20f, 20 };
-    public static float[] CameraPerspectiveHandClampY = { -20, 20f }; // X = -8, 1 || Y = Initial default -8, 1
+    public static float[] CameraPerspectiveHandClampX = { -20f, 20 },
+    CameraPerspectiveHandClampY = { -20, 20f }; // X = -8, 1 || Y = Initial default -8, 1
     public const float CameraFollowInterpolation = 0.034f;
 
     //UI : Camera
-    public const int ConstDefaultCameraWidth = 1500;
-    public const int ConstDefaultCameraHeight = 1600;
+    public const int ConstDefaultCameraWidth = 1500,
+    ConstDefaultCameraHeight = 1600;
 
     //UI: Buttons listeners
     //UI: Editor tools
-    public const string DebugStartButton = "GridDebugButton";
-    public const string GridDebugContent = "GridDebug";
-    public const string GridDisplay = "GridDisplay";
-    public const string MainContainer = "MainContainer";
-    public const string ClientContainerGraphDebuger = "ClientContainerGraphDebuger";
-    public const string EmployeeContainerGraphDebuger = "EmployeeContainerGraphDebuger";
-    public const string ComboBoxContainer = "ComboBoxContainer";
-    public const string GraphLevel = "GraphLevel";
-    public const string NODE = "NODE";
-    public const string EMPLOYEE_PREFIX = "EMPLOYEE";
-    public const string IsometricWorldDebugUI = "Assets/Editor/IsometricWorldDebug.uxml";
-    public const string IsometricWorldDebugUIStyles = "Assets/Editor/IsometricWorldDebug.uss";
-    public const string IsometricWorldDebugUIStateMachine = "Assets/Editor/StatemachineWorldDebug.uxml";
-    public const string IsometricWorldDebugUIStylesStateMachine = "Assets/Editor/StatemachineWorldDebug.uss";
+    public const string DebugStartButton = "GridDebugButton",
+    GridDebugContent = "GridDebug",
+    GridDisplay = "GridDisplay",
+    MainContainer = "MainContainer",
+    ClientContainerGraphDebuger = "ClientContainerGraphDebuger",
+    EmployeeContainerGraphDebuger = "EmployeeContainerGraphDebuger",
+    ComboBoxContainer = "ComboBoxContainer",
+    GraphLevel = "GraphLevel",
+     NODE = "NODE",
+    EMPLOYEE_PREFIX = "EMPLOYEE",
+    IsometricWorldDebugUI = "Assets/Editor/IsometricWorldDebug.uxml",
+    IsometricWorldDebugUIStyles = "Assets/Editor/IsometricWorldDebug.uss",
+    IsometricWorldDebugUIStateMachine = "Assets/Editor/StatemachineWorldDebug.uxml",
+    IsometricWorldDebugUIStylesStateMachine = "Assets/Editor/StatemachineWorldDebug.uss";
 
     //SCENE
     public const string MainCamera = "MainCamera";
 
     //Tiles
-    public const string GridTilesSimple = "Grid/Tiles/GridTile";
-    public const string GridTilesHighlightedFloor = "Grid/Tiles/HighlightedFloor@3x";
+    public const string GridTilesSimple = "Grid/Tiles/GridTile",
+    GridTilesHighlightedFloor = "Grid/Tiles/HighlightedFloor@3x";
 
     // Grid Config
     public const float GridCellSize = 0.25f; // 0.25f default
-    public const int GridWidth = 25; // Number of cell per Grid CellSize
-    public const int GridHeight = 20;
-    public const int GridStartX = 0;
-    public const int GrtGridStartY = 0;
+    public const int GridWidth = 25, // Number of cell per Grid CellSize
+    GridHeight = 20,
+    GridStartX = 0,
+    GrtGridStartY = 0;
 }
