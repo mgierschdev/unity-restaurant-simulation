4fc56b1eeb4974a53e08bb7b6e7fa34cb49f1ffa
pacmax2 <mgierschdev@gmail.com>
2022-11-19 12:03:18 -0400

    refactor DataGameUser

--- Assets/Scripts/Game/Players/Model/DataGameObject.cs
+++ Assets/Scripts/Game/Players/Model/DataGameObject.cs
@@ -2,7 +2,7 @@ using System;
 using UnityEngine;
 
 [Serializable]
-public class DataGameObject : MonoBehaviour
+public class DataGameObject
 {
     [SerializeField]
     public int ID;//StoreItemType
--- Assets/Scripts/Tests/EditMode/TestJSON.cs
+++ Assets/Scripts/Tests/EditMode/TestJSON.cs
@@ -1,6 +1,6 @@
 using System;
-using NUnit.Framework;
 using System.Collections.Generic;
+using NUnit.Framework;
 
 public class TestJSON
 {
@@ -8,6 +8,13 @@ public class TestJSON
     public void TestSaveJSON()
     {
         DataGameUser user = PlayerData.GetNewUser();
+        user.SaveToJSONFileAsync();
 
+        string[] files = UtilSaveFile.GetSaveFiles();
+        GameLog.Log("Loading user from file " + files[0]);
+        DataGameUser loadUser = DataGameUser.CreateFromJSON(files[0]);
+        GameLog.Log("User created " + loadUser.NAME + " " + loadUser.INTERNAL_ID);
+
+        
     }
 }
