0d6237383b589c60267edfb9654095c516b4d4c9
pacmax2 <mgierschdev@gmail.com>
2022-11-21 16:16:52 -0400

    Refactor adding AnimatorState as an Enum and adding new animation to the animation state controller

--- Assets/Scripts/Util/NPCAnimatorState.cs
+++
@@ -1,7 +0,0 @@
-public static class NPCAnimatorState
-{
-    public const string Walking = "Walking";
-    public const string Idle = "Idle";
-    public const string WaitingAtTable = "WaitingAtTable";
-    public const string WalkingToTable = "WalkingToTable";
-}
