3799ff80be9e98678ed6f983927c51238d9aded3
pacmax2 <mgierschdev@gmail.com>
2022-12-01 08:38:50 -0400

    adding upgrade type into StoreItemType

--- Assets/Scripts/Game/Players/StoreGameObject.cs
+++ Assets/Scripts/Game/Players/StoreGameObject.cs
@@ -7,20 +7,18 @@ public class StoreGameObject : IEquatable<StoreGameObject>, IComparable<StoreGam
     public int Cost { get; set; }
     public ObjectType Type { get; set; }
     public StoreItemType StoreItemType { get; set; }
-    public UpgradeType UpgradeType { get; set; }
     public string MenuItemSprite { get; set; }
     public string SpriteLibCategory { get; set; }
     public string PrefabLocation { get; set; }
     public bool HasActionPoint { get; set; }
 
-    public StoreGameObject(string name, string identifier, ObjectType type, StoreItemType tableType, UpgradeType upgradeType, string categorySprite, string prefabLocation, int cost, bool hasActionPoint)
+    public StoreGameObject(string name, string identifier, ObjectType type, StoreItemType tableType, string categorySprite, string prefabLocation, int cost, bool hasActionPoint)
     {
         Identifier = identifier;
         Name = name;
         Cost = cost;
         Type = type;
         StoreItemType = tableType;
-        UpgradeType = upgradeType;
         MenuItemSprite = Settings.StoreSpritePath + identifier;
         SpriteLibCategory = categorySprite;
         PrefabLocation = prefabLocation;
