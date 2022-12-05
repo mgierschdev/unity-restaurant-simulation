a8b1e92126b50dd4b1e826d39c948c53525a23c4
pacmax2 <mgierschdev@gmail.com>
2022-12-05 10:13:02 -0400

    doing UpdatePosition on the awake method of GameObjectMovementBase

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -11,7 +11,7 @@ public static class Settings
     public const bool devEnv = false;
 #endif
 
-    public const int MaxNpcNumber = 10; // 100 > fps 11-12
+    public const int MaxNpcNumber = 1; // 100 > fps 11-12
     public static int[] StartContainer = new int[] { 4, 13 },
     StartTable = new int[] { 4, 11 },
     StartCounter = new int[] { 5, 12 };
