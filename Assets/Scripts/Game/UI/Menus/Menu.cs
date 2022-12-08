69765c24b19fa6186376c80a4ab60c55ae9f8b28
pacmax2 <mgierschdev@gmail.com>
2022-12-08 12:07:23 -0400

    isable storage button if storage is empty

--- Assets/Scripts/Game/UI/Menus/Menu.cs
+++ Assets/Scripts/Game/UI/Menus/Menu.cs
@@ -1,11 +1,22 @@
+using UnityEngine.UI;
+
 public class MenuItem
 {
     private MenuType type;
     private MenuTab menuTab;
+    private Button tabButton;
     public string name;
 
     public MenuItem(MenuTab menuTab, MenuType type, string name)
     {
+        this.menuTab = menuTab;
+        this.name = name;
+        this.type = type;
+    }
+
+    public MenuItem(MenuTab menuTab, MenuType type, string name, Button tabButton)
+    {
+        this.tabButton = tabButton;
         this.menuTab = menuTab;
         this.name = name;
         this.type = type;
@@ -19,5 +30,10 @@ public class MenuItem
     public MenuTab GetMenuTab()
     {
         return menuTab;
+    }
+
+    public Button GetTabButton()
+    {
+        return tabButton;
     }
 }
