13714cc7cd3751f22b5eab6cb84ae50ce6ba708c
pacmax2 <mgierschdev@gmail.com>
2023-01-04 14:19:37 -0400

    Fixing Tile map floor 3

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -3,7 +3,7 @@ public static class Settings
     // General Config
     public const bool CellDebug = false;
     public const int DebugTextSize = 9;
-    public const float NpcDefaultMovementSpeed = 1f; //1 = Looks good with current animation
+    public const float NpcDefaultMovementSpeed = 1.3f; //1 = Looks good with current animation
 
 #if UNITY_EDITOR
     public const bool devEnv = true;
@@ -11,7 +11,7 @@ public static class Settings
     public const bool devEnv = false;
 #endif
 
-    public const int MaxNpcNumber = 30; // 100 > fps 11-12
+    public const int MaxNpcNumber = 10; // 100 > fps 11-12
     public static int[] StartStoreItemDispenser = new int[] { 5, 14 },
     StartTable = new int[] { 5, 12 },
     StartCounter = new int[] { 3, 13 };
