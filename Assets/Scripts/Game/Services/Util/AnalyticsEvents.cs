027358629d7c43983682a3855343d7f91656eb36
pacmax2 <mgierschdev@gmail.com>
2023-01-05 15:24:04 -0400

    removing static from SavePlayerDataResponse enum

--- Assets/Scripts/Game/Services/Util/AnalyticsEvents.cs
+++ Assets/Scripts/Game/Services/Util/AnalyticsEvents.cs
@@ -4,7 +4,7 @@ public static class AnalyticsEvents
 {
     public static string SavePlayerData = "SavePlayerData";
 }
-public static enum SavePlayerDataResponse
+public enum SavePlayerDataResponse
 {
     NEW_PLAYER_SAVED = 1,
     ERROR_WHILE_SAVING = 2,
