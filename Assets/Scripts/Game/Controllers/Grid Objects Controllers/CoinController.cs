7c968474e7bb9bf4eb6b22d430c7298ca230c981
pacmax2 <mgierschdev@gmail.com>
2023-01-15 10:19:36 -0400

    Refactor name Math as Math extended since the namespace is already in use

--- Assets/Scripts/Game/Controllers/Grid Objects Controllers/CoinController.cs
+++ Assets/Scripts/Game/Controllers/Grid Objects Controllers/CoinController.cs
@@ -4,35 +4,37 @@ public class CoinController : MonoBehaviour
 {
     private bool consume;
     private Vector3 targetPosition;
-    [SerializeField]
-    private float coinSpeed = 4f;
+    private float coinSpeed = 0.1f;
+    private Vector3 startPoint;
+    private float interpolationTime;
 
     private void Awake()
     {
         consume = false;
         transform.name = BussGrid.GetObjectID(ObjectType.COIN);
+        startPoint = transform.position;
+        interpolationTime = 0;
+
     }
 
     private void Update()
     {
         if (consume)
         {
-            transform.position = Vector3.Lerp(transform.position, targetPosition, coinSpeed * Time.fixedDeltaTime);
+            interpolationTime += Time.deltaTime * 0.1f;
+            transform.position = MathExtended.EaseTest(interpolationTime, startPoint, targetPosition);
+
+            //Vector3.Lerp(transform.position, targetPosition, coinSpeed * Time.fixedDeltaTime);
             CheckIfAtTarget();
         }
         else if (Input.GetMouseButtonDown(0) && IsClickingSelf())
         {
             Vector3 target = Camera.main.ScreenToWorldPoint(PlayerData.GetMoneyTextPosition());
-            SetTargetPosition(target);
+            targetPosition = target;
             // TODO: Make it follow the reference of the actual gameObject rather than the coord in that way it will follow the coord even is the player moves with the perspective hand
             // Use some kind of lerp or curve easing 
             consume = true;
         }
-    }
-
-    private void SetTargetPosition(Vector3 target)
-    {
-        targetPosition = target;
     }
 
     private void CheckIfAtTarget()
---
+++ Assets/Scripts/Util/MathExtended.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+public static class Math
+{
+
+
+    // returns the value of a Bazier curve given 3 parameters 
+    // The parameter  a/b start/finish and c/d tangent c/d
+    // Definition: https://en.wikipedia.org/wiki/Bézier_curve
+    // Scale 0 - 1 where (0,0) is the left bot place on the screen and (1,1) the top right corner
+
+    public static Vector2 CubicBazier(float time, Vector3 start, Vector2 end, Vector3 tangent1, Vector3 tangent2)
+    {
+        // x = (1 - t) * (1 - t) * p[0].x + 2 * (1 - t) * t * p[1].x + t * t * p[2].x;
+        // y = (1 - t) * (1 - t) * p[0].y + 2 * (1 - t) * t * p[1].y + t * t * p[2].y;
+        float cx = 3 * (tangent1.x - start.x);
+        float cy = 3 * (tangent1.y - start.y);
+
+        float bx = 3 * (tangent2.x - tangent1.x) - cx;
+        float by = 3 * (tangent2.y - tangent1.y) - cy;
+
+        float ax = end.x - start.x - cx - bx;
+        float ay = end.y - start.y - cy - by;
+
+        float Cube = time * time * time;
+        float Square = time * time;
+
+        float resX = (ax * Cube) + (bx * Square) + (cx * time) + start.x;
+        float resY = (ay * Cube) + (by * Square) + (cy * time) + start.y;
+
+        return new Vector2(resX, resY);
+    }
+
+    // Generating an ease animation
+    public static Vector2 EaseTest(float t, Vector3 start, Vector3 end)
+    {
+        return CubicBazier(t, start, end, new Vector3(0, 1), new Vector3(1, 0));
+    }
+
+
+}
