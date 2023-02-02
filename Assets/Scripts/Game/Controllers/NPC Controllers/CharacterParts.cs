ac5b0ab90aec878942c19ddd44e24eadb5b5f7a6
pacmax2 <mgierschdev@gmail.com>
2023-02-02 14:48:48 -0400

    Adding Character class, Character parts, character enum and logic to the SkinSelectorController

---
+++ Assets/Scripts/Game/Controllers/NPC Controllers/CharacterParts.cs
@@ -0,0 +1,21 @@
+public static class CharacterParts
+{
+    public static string Head = "Head-1";
+    public static string Body = "Body-1";
+    public static string ArmRight = "Arm-Right-1";
+    public static string ArmLeft = "Arm-Left-1";
+    public static string Waist = "Waist-1";
+    public static string FootLeft = "Foot-Left-1";
+    public static string FootRight = "Foot-Right-1";
+    public static string LegRight = "Leg-Right-1";
+    public static string LegLeft = "Leg-Left-1";
+}
+
+public enum CharacterType
+{
+    employee = 1,
+    client_1 = 2,
+    client_2 = 3,
+    client_3 = 4,
+    client_4 = 5
+}
