cb14d02e20e627737542b33da72664fe83470d46
“mgierschdev <mgierschded@gmail.com>
2023-07-04 18:26:37 -0400

    Last working version of the project as it is, 4 Jul

---
+++ Assets/Scripts/Game/Controllers/NPC Controllers/NPCController.cs
@@ -0,0 +1,203 @@
+using System;
+using UnityEngine;
+using IEnumerator = System.Collections.IEnumerator;
+using Random = UnityEngine.Random;
+
+// Controls NPCs players
+// Attached to: NPC Objects
+public class NPCController : GameObjectMovementBase
+{
+    [SerializeField]
+    private const float MaxStateTime = 120; // 2min
+    [SerializeField]
+    private NpcState state;//TODO: for debug
+
+    private void Start()
+    {
+        type = ObjectType.CLIENT;
+        SetID();
+        stateMachine = NPCStateMachineFactory.GetClientStateMachine(Name);
+        StartCoroutine(UpdateTransitionStates());
+    }
+
+    private void FixedUpdate()
+    {
+        try
+        {
+            UpdatePosition();
+            UpdateTimeInState();
+            UpdateTargetMovement();
+            UpdateAnimation();
+        }
+        catch (Exception e)
+        {
+            GameLog.LogWarning("Exception thrown, likely missing reference (FixedUpdate NPCController): " + e);
+            stateMachine.SetTransition(NpcStateTransitions.TABLE_MOVED);
+        }
+    }
+
+    public IEnumerator UpdateTransitionStates()
+    {
+        for (; ; )
+        {
+            if (!IsMoving())
+            {
+                CheckIfAtTarget();
+                CheckUnrespawn();
+                CheckIfTableHasBeenAssigned();
+                Wander();
+                CheckIfTableMoved();
+                state = stateMachine.Current.State;
+                stateMachine.CheckTransition();
+                MoveNPC();
+            }
+
+            yield return new WaitForSeconds(2f);
+        }
+    }
+
+    private void MoveNPC()
+    {
+        if (stateMachine.Current.State == NpcState.WALKING_UNRESPAWN && !stateMachine.GetTransitionState(NpcStateTransitions.MOVING_TO_UNSRESPAWN))
+        {
+            stateMachine.SetTransition(NpcStateTransitions.MOVING_TO_UNSRESPAWN);
+            GoTo(BussGrid.GetRandomSpamPointWorldPosition().GridPosition);
+        }
+        else if (stateMachine.Current.State == NpcState.WANDER)
+        {
+            GoTo(BussGrid.GetRandomWalkablePosition(Position));
+        }
+        else if (stateMachine.Current.State == NpcState.WALKING_TO_TABLE)
+        {
+            if (table == null) { return; }
+            GoTo(table.GetActionTileInGridPosition());
+        }
+    }
+
+    private void CheckIfTableMoved()
+    {
+        if (stateMachine.Current.State == NpcState.WAITING_TO_BE_ATTENDED && table == null)
+        {
+            stateMachine.SetTransition(NpcStateTransitions.TABLE_MOVED);
+        }
+    }
+
+    private void CheckUnrespawn()
+    {
+        if (!stateMachine.GetTransitionState(NpcStateTransitions.WALK_TO_UNRESPAWN))
+        {
+            if (stateTime >= MaxStateTime ||
+                stateMachine.GetTransitionState(NpcStateTransitions.TABLE_MOVED) ||
+                stateMachine.GetTransitionState(NpcStateTransitions.ATTENDED))
+            {
+                stateMachine.SetTransition(NpcStateTransitions.WALK_TO_UNRESPAWN);
+                stateMachine.SetTransition(NpcStateTransitions.TABLE_MOVED);
+            }
+        }
+    }
+
+    private void Wander()
+    {
+        // Chance to no wander
+        float randT = Random.Range(0, 8);
+
+        if (stateMachine.Current.State != NpcState.IDLE || randT > 2)
+        {
+            stateMachine.UnSetTransition(NpcStateTransitions.WANDER);
+
+            if (stateMachine.Current.State == NpcState.WANDER)
+            {
+                stateMachine.SetTransition(NpcStateTransitions.WANDER_TO_IDLE);
+            }
+        }
+        else
+        {
+            stateMachine.UnSetTransition(NpcStateTransitions.WANDER_TO_IDLE);
+            stateMachine.SetTransition(NpcStateTransitions.WANDER);
+        }
+    }
+
+    private void CheckIfTableHasBeenAssigned()
+    {
+        if (table != null)
+        {
+            stateMachine.SetTransition(NpcStateTransitions.TABLE_AVAILABLE);
+        }
+        else
+        {
+            stateMachine.UnSetTransition(NpcStateTransitions.TABLE_AVAILABLE);
+        }
+    }
+
+    private void CheckIfAtTarget()
+    {
+        if (!(currentTargetGridPosition.x == Position.x && currentTargetGridPosition.y == Position.y))
+        {
+            return;
+        }
+
+        if (stateMachine.Current.State == NpcState.WALKING_UNRESPAWN)
+        {
+            BussGrid.GameController.RemoveNpc(this);
+
+            if (table != null)
+            {
+                table.FreeObject();
+                table = null;
+            }
+            Destroy(gameObject);
+        }
+        else if (stateMachine.Current.State == NpcState.WALKING_TO_TABLE)
+        {
+            stateMachine.SetTransition(NpcStateTransitions.WAITING_AT_TABLE);
+        }
+    }
+
+    public NpcState GetNpcState()
+    {
+        return stateMachine.Current.State;
+    }
+
+    public float GetNpcStateTime()
+    {
+        return Mathf.Floor(stateTime);
+    }
+
+    public GameGridObject GetTable()
+    {
+        return table;
+    }
+
+    public void SetTable(GameGridObject obj)
+    {
+        table = obj;
+    }
+
+    public void FlipTowards(Vector3Int direction)
+    {
+        StandTowards(direction);
+    }
+
+    public bool HasTable()
+    {
+        return table != null;
+    }
+
+    public void SetTableMoved()
+    {
+        table = null;
+        stateMachine.SetTransition(NpcStateTransitions.TABLE_MOVED);
+        stateMachine.SetTransition(NpcStateTransitions.WALK_TO_UNRESPAWN);
+    }
+
+    public void SetAttended()
+    {
+        stateMachine.SetTransition(NpcStateTransitions.ATTENDED);
+        stateMachine.SetTransition(NpcStateTransitions.ORDER_SERVED);
+    }
+
+    public void SetBeingAttended()
+    {
+        stateMachine.SetTransition(NpcStateTransitions.BEING_ATTENDED);
+    }
+}
---
+++ Assets/Scripts/Game/Players/MenuObjectList.cs
@@ -0,0 +1,172 @@
+using System.Collections.Generic;
+
+public static class MenuObjectList
+{
+    public static List<StoreGameObject> ActionPointItems;
+    public static List<StoreGameObject> CounterItems;
+    public static List<StoreGameObject> ContainerItems;
+    public static List<StoreGameObject> BaseDispenserList;
+    public static List<StoreGameObject> AllStoreItems;
+    public static List<StoreGameObject> InGameStoreItems;
+    public static List<StoreGameObject> EmployeeItems;
+    public static List<StoreGameObject> SettingsItems;
+    public static List<StoreGameObject> Storage;
+    //Object Sprite Library Identifier / StoreObject
+    public static Dictionary<string, StoreGameObject> StoreItemDictionary;
+    //Object Sprite Library Identifier / StoreObject
+    public static Dictionary<StoreItemType, StoreGameObject> StoreItemTypeDic;
+
+    public static void Init()
+    {
+        SetAllItems();
+    }
+    public static void SetAllItems()
+    {
+        ActionPointItems = new List<StoreGameObject>();
+        CounterItems = new List<StoreGameObject>();
+        BaseDispenserList = new List<StoreGameObject>();
+        ContainerItems = new List<StoreGameObject>();
+        InGameStoreItems = new List<StoreGameObject>();
+        SettingsItems = new List<StoreGameObject>();
+        EmployeeItems = new List<StoreGameObject>();
+        Storage = new List<StoreGameObject>();
+
+        StoreItemDictionary = new Dictionary<string, StoreGameObject>();
+        StoreItemTypeDic = new Dictionary<StoreItemType, StoreGameObject>();
+
+        AllStoreItems = new List<StoreGameObject>{
+            new StoreGameObject("Wooden table", "SingleTable-1", ObjectType.NPC_SINGLE_TABLE, StoreItemType.TABLE_SINGLE_1, Settings.SpriteLibCategoryTables, Settings.PrefabSingleTable, 20, true),
+            new StoreGameObject("Wooden squared table", "SingleTable-2", ObjectType.NPC_SINGLE_TABLE, StoreItemType.TABLE_SINGLE_2, Settings.SpriteLibCategoryTables, Settings.PrefabSingleTable, 40, true),
+            // new StoreGameObject("Wooden table", "SingleTable-3", ObjectType.NPC_SINGLE_TABLE, StoreItemType.TABLE_SINGLE_3, Settings.SpriteLibCategoryTables, Settings.PrefabSingleTable, 50, true),
+            // new StoreGameObject("Dark wood table", "SingleTable-4", ObjectType.NPC_SINGLE_TABLE, StoreItemType.TABLE_SINGLE_4, Settings.SpriteLibCategoryTables, Settings.PrefabSingleTable, 60, true),
+            // new StoreGameObject("Wooden table", "SingleTable-5", ObjectType.NPC_SINGLE_TABLE, StoreItemType.TABLE_SINGLE_5, Settings.SpriteLibCategoryTables, Settings.PrefabSingleTable, 70, true),
+            // new StoreGameObject("Dark wood table", "SingleTable-6", ObjectType.NPC_SINGLE_TABLE, StoreItemType.TABLE_SINGLE_6, Settings.SpriteLibCategoryTables, Settings.PrefabSingleTable, 80, true),
+            // new StoreGameObject("Wooden table", "SingleTable-7", ObjectType.NPC_SINGLE_TABLE, StoreItemType.TABLE_SINGLE_7, Settings.SpriteLibCategoryTables, Settings.PrefabSingleTable, 90, true),
+            // new StoreGameObject("Dark wood table", "SingleTable-8", ObjectType.NPC_SINGLE_TABLE, StoreItemType.TABLE_SINGLE_8, Settings.SpriteLibCategoryTables, Settings.PrefabSingleTable, 100, true),
+            // new StoreGameObject("Iron table", "SingleTable-9", ObjectType.NPC_SINGLE_TABLE, StoreItemType.TABLE_SINGLE_9, Settings.SpriteLibCategoryTables, Settings.PrefabSingleTable, 200, true),
+            // new StoreGameObject("Iron table", "SingleTable-10", ObjectType.NPC_SINGLE_TABLE, StoreItemType.TABLE_SINGLE_10, Settings.SpriteLibCategoryTables, Settings.PrefabSingleTable, 200, true),
+            new StoreGameObject("Counter", "Counter-1", ObjectType.NPC_COUNTER, StoreItemType.COUNTER_1, Settings.SpriteLibCategoryStoreObjects, Settings.PrefabCounter, 50, true),
+
+            new StoreGameObject("Wooden container", "Dispenser-1", ObjectType.STORE_ITEM, StoreItemType.SNACK_MACHINE_1, Settings.SpriteLibCategoryStoreItems, Settings.PrefabSnackMachine, 40, false),
+
+            new StoreGameObject("UNDEFINED", "UNDEFINED", ObjectType.UNDEFINED, StoreItemType.UNDEFINED, "UNDEFINED", "UNDEFINED", 999, false)
+        };
+
+        foreach (StoreGameObject storeItem in AllStoreItems)
+        {
+            StoreItemDictionary.Add(storeItem.Identifier, storeItem);
+            StoreItemTypeDic.Add(storeItem.StoreItemType, storeItem);
+
+            if (storeItem.HasActionPoint)
+            {
+                ActionPointItems.Add(storeItem);
+            }
+
+            if (storeItem.Type == ObjectType.STORE_ITEM)
+            {
+                BaseDispenserList.Add(storeItem);
+            }
+        }
+
+        ActionPointItems.Sort();
+        BaseDispenserList.Sort();
+    }
+    // The id = StoreGameObject.Identifier
+    public static StoreGameObject GetStoreObject(string id)
+    {
+        if (!StoreItemDictionary.ContainsKey(id))
+        {
+            GameLog.LogWarning("Storeitem with id " + id + " does not exist");
+            return null;
+        }
+        return StoreItemDictionary[id];
+    }
+
+    public static StoreGameObject GetStoreObject(StoreItemType storeItem)
+    {
+        if (!StoreItemTypeDic.ContainsKey(storeItem))
+        {
+            GameLog.LogWarning("Storeitem with id " + storeItem + " does not exist");
+            return null;
+        }
+        return StoreItemTypeDic[storeItem];
+    }
+
+    public static ItemType GetItemGivenDispenser(StoreItemType type)
+    {
+        switch (type)
+        {
+            case StoreItemType.SNACK_MACHINE_1: return ItemType.ORANGE_JUICE;
+        }
+        return ItemType.UNDEFINED;
+    }
+
+    public static string GetItemSprite(ItemType type)
+    {
+        switch (type)
+        {
+            case ItemType.ORANGE_JUICE: return "Dispenser-1";
+        }
+        return "";
+    }
+
+    public static string GetPrefab(StoreItemType type)
+    {
+        StoreGameObject obj = GetStoreObject(type);
+        switch (obj.StoreItemType)
+        {
+            case StoreItemType.TABLE_SINGLE_1: return Settings.PrefabSingleTable;
+            case StoreItemType.TABLE_SINGLE_2: return Settings.PrefabSingleTable;
+            case StoreItemType.TABLE_SINGLE_3: return Settings.PrefabSingleTable;
+            case StoreItemType.TABLE_SINGLE_4: return Settings.PrefabSingleTable;
+            case StoreItemType.TABLE_SINGLE_5: return Settings.PrefabSingleTable;
+            case StoreItemType.TABLE_SINGLE_6: return Settings.PrefabSingleTable;
+            case StoreItemType.TABLE_SINGLE_7: return Settings.PrefabSingleTable;
+            case StoreItemType.TABLE_SINGLE_8: return Settings.PrefabSingleTable;
+            case StoreItemType.TABLE_SINGLE_9: return Settings.PrefabSingleTable;
+            case StoreItemType.COUNTER_1: return Settings.PrefabCounter;
+            case StoreItemType.SNACK_MACHINE_1: return Settings.PrefabSnackMachine;
+        }
+        return "";
+    }
+
+    public static List<StoreGameObject> GetItemList(MenuTab tab)
+    {
+        switch (tab)
+        {
+            case MenuTab.STORE_ITEMS: return ActionPointItems;
+            case MenuTab.STORAGE_TAB: return BaseDispenserList;
+            // case MenuTab.IN_GAME_STORE_TAB: return InGameStoreItems;
+            case MenuTab.SETTINGS_TAB: return SettingsItems;
+        }
+
+        return new List<StoreGameObject>();
+    }
+
+    public static string GetButtonLabel(MenuTab tab)
+    {
+        switch (tab)
+        {
+            case MenuTab.TABLES_TAB: return "Tables";
+            case MenuTab.SNACKS_TAB: return "Dispensers";
+            case MenuTab.UPGRADE: return "Research";
+            case MenuTab.STORAGE_TAB: return "Storage";
+            case MenuTab.SETTINGS_TAB: return "Settings";
+        }
+        return "";
+    }
+
+    public static List<DataGameObject> LoadCurrentUserStorage()
+    {
+        List<DataGameObject> storage = new List<DataGameObject>();
+
+        foreach (DataGameObject obj in PlayerData.GetDataGameUser().OBJECTS)
+        {
+            if (obj.IS_STORED)
+            {
+                storage.Add(obj);
+            }
+        }
+        return storage;
+    }
+}
